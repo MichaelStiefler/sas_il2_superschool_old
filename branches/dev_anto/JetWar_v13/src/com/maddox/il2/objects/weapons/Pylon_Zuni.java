5f8a24929a12c34838f30296cbfd5c088e0a82a4
SAS~Anto <SAS~Anto@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2013-01-17 16:16:11 +0000

    Source code for Aviator Mod Team's Il-2 1956 Project

---
+++ branches/dev_anto/JetWar_v13/src/com/maddox/il2/objects/weapons/Pylon_Zuni.java
@@ -0,0 +1,16 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class Pylon_Zuni extends Pylon
+{
+
+    public Pylon_Zuni()
+    {
+    }
+
+    static 
+    {
+        Property.set(com.maddox.il2.objects.weapons.Pylon_Zuni.class, "mesh", "3DO/Arms/Pylon_Zuni/mono.sim");
+    }
+}
