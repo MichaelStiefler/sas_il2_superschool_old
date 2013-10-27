18c6a8bab99316ce59cc83735fa3ea8e652f39b9
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2013-10-27 14:39:38 +0000

    4.12.2m initial commit

---
+++ Jugs Reloaded/src/com/maddox/il2/objects/weapons/FuelTank_Tank165galP47.java
@@ -0,0 +1,16 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class FuelTank_Tank165galP47 extends FuelTank {
+
+	public FuelTank_Tank165galP47() {
+	}
+
+	static {
+		Class class1 = FuelTank_Tank165galP47.class;
+		Property.set(class1, "mesh", "3DO/Arms/Tank165galP47/mono.sim");
+		Property.set(class1, "kalibr", 0.6F);
+		Property.set(class1, "massa", 477F);
+	}
+}
---
+++ Jugs Reloaded/src/com/maddox/il2/objects/weapons/WeaponHelper.java
@@ -0,0 +1,17 @@
+package com.maddox.il2.objects.weapons;
+
+public class WeaponHelper {
+
+	public static void setRocketGunShotStep(RocketGun theRocketGun, int theShotStep) {
+		theRocketGun.shotStep = theShotStep;
+	}
+
+	public static void setBombGunShotStep(BombGun theBombGun, int theShotStep) {
+		theBombGun.shotStep = theShotStep;
+	}
+
+	public static void setRocketBombGunShotStep(RocketBombGun theRocketBombGun, int theShotStep) {
+		theRocketBombGun.shotStep = theShotStep;
+	}
+
+}
