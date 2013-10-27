c8b321621defd9a13fe7837edf7d8fb95c7dd5a6
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2013-10-27 14:37:06 +0000

    4.12.2m initial commit

---
+++ InfoMod FullAuto/src/com/maddox/il2/objects/air/WeaponsHelper.java
@@ -0,0 +1,7 @@
+package com.maddox.il2.objects.air;
+
+public class WeaponsHelper {
+	public static void weaponsLoad(Aircraft aircraft, String s) throws Exception {
+		aircraft.weaponsLoad(s);
+	}
+}
