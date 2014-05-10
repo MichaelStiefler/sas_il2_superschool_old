3a81fb7d9b38d0980413379ae1234abc4c7f4ccd
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2014-05-10 17:10:07 +0000

    Guided Missiles Consolidation WIP 0.01

---
+++ Guided Missiles/src/com/maddox/il2/objects/weapons/MissileR3R.java
@@ -0,0 +1,88 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.JGP.Color3f;
+import com.maddox.JGP.Point3d;
+import com.maddox.il2.engine.Actor;
+import com.maddox.il2.engine.Orient;
+import com.maddox.rts.NetChannel;
+import com.maddox.rts.Property;
+import com.maddox.rts.Spawn;
+
+public class MissileR3R extends Missile {
+	static class SPAWN extends Missile.SPAWN {
+
+		SPAWN() {
+		}
+
+		public void doSpawn(Actor actor, NetChannel netchannel, int i, Point3d point3d, Orient orient, float f) {
+			new MissileR3R(actor, netchannel, i, point3d, orient, f);
+		}
+	}
+
+	static {
+		Class class1 = MissileR3R.class;
+		Property.set(class1, "mesh", "3do/arms/R-3R/mono.sim");
+		Property.set(class1, "sprite", "3do/effects/RocketSidewinder/RocketSidewinderSpriteBlack.eff");
+		Property.set(class1, "flame", "3do/Effects/RocketSidewinder/RocketSidewinderFlame.sim");
+		Property.set(class1, "smoke", "3do/Effects/RocketSidewinder/RocketSidewinderSmoke.eff");
+		Property.set(class1, "emitColor", new Color3f(1.0F, 1.0F, 0.5F));
+		Property.set(class1, "emitLen", 50F);
+		Property.set(class1, "emitMax", 0.4F);
+		Property.set(class1, "sound", "weapon.rocket_132");
+		Property.set(class1, "timeLife", 35F);
+		Property.set(class1, "timeFire", 2.4F);
+		Property.set(class1, "force", 12000F);
+		Property.set(class1, "forceT1", 0.7F);
+		Property.set(class1, "forceP1", 0.0F);
+		Property.set(class1, "forceT2", 0.2F);
+		Property.set(class1, "forceP2", 50F);
+		Property.set(class1, "dragCoefficient", 0.3F);
+		Property.set(class1, "powerType", 0);
+		Property.set(class1, "power", 0.42F);
+		Property.set(class1, "radius", 8.96F);
+		Property.set(class1, "kalibr", 0.2F);
+		Property.set(class1, "massa", 90F);
+		Property.set(class1, "massaEnd", 63F);
+		Property.set(class1, "stepMode", 0);
+		Property.set(class1, "launchType", 2);
+		Property.set(class1, "detectorType", 2);
+		Property.set(class1, "sunRayAngle", 0.0F);
+		Property.set(class1, "multiTrackingCapable", 1);
+		Property.set(class1, "canTrackSubs", 0);
+		Property.set(class1, "minPkForAI", 25F);
+		Property.set(class1, "timeForNextLaunchAI", 10000L);
+		Property.set(class1, "engineDelayTime", 0L);
+		Property.set(class1, "attackDecisionByAI", 1);
+		Property.set(class1, "targetType", 1);
+		Property.set(class1, "shotFreq", 1.01F);
+		Property.set(class1, "groundTrackFactor", 10F);
+		Property.set(class1, "flareLockTime", 1000L);
+		Property.set(class1, "trackDelay", 1000L);
+		Property.set(class1, "failureRate", 30F);
+		Property.set(class1, "maxLockGForce", 99.9F);
+		Property.set(class1, "maxFOVfrom", 25F);
+		Property.set(class1, "maxFOVto", 180F);
+		Property.set(class1, "PkMaxFOVfrom", 30F);
+		Property.set(class1, "PkMaxFOVto", 70F);
+		Property.set(class1, "PkDistMin", 1000F);
+		Property.set(class1, "PkDistOpt", 3000F);
+		Property.set(class1, "PkDistMax", 8000F);
+		Property.set(class1, "maxSpeed", 2012.214F);
+		Property.set(class1, "leadPercent", 100F);
+		Property.set(class1, "maxGForce", 11F);
+		Property.set(class1, "stepsForFullTurn", 10);
+		Property.set(class1, "fxLock", "weapon.K5.lock");
+		Property.set(class1, "fxNoLock", "weapon.K5.nolock");
+		Property.set(class1, "smplLock", "K5_lock.wav");
+		Property.set(class1, "smplNoLock", "K5_no_lock.wav");
+		Property.set(class1, "friendlyName", "R-3R");
+		Spawn.add(class1, new SPAWN());
+	}
+
+	public MissileR3R() {
+	}
+
+	public MissileR3R(Actor actor, NetChannel netchannel, int i, Point3d point3d, Orient orient, float f) {
+		this.MissileInit(actor, netchannel, i, point3d, orient, f);
+	}
+}
---
+++ Guided Missiles/src/com/maddox/il2/objects/weapons/RocketGunAIM9B.java
@@ -0,0 +1,16 @@
+// Source File Name: RocketGunAIM9B.java
+// Author:           Storebror
+// Last Modified by: Storebror 2011-06-01
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class RocketGunAIM9B extends MissileGun implements RocketGunWithDelay {
+	static {
+		Class class1 = RocketGunAIM9B.class;
+		Property.set(class1, "bulletClass", (Object) MissileAIM9B.class);
+		Property.set(class1, "bullets", 1);
+		Property.set(class1, "shotFreq", 0.25F);
+		Property.set(class1, "sound", "weapon.rocketgun_132");
+	}
+}
