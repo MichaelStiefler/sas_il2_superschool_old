1109f8becc32eaad86645e6e8e4f2db009ad573e
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2014-06-09 09:47:08 +0000

    Initial AH-1G Weapons Commit

---
+++ AH-1G Weapons/src/com/maddox/il2/objects/weapons/PylonM158.java
@@ -0,0 +1,13 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class PylonM158 extends PylonRO_82_1 {
+
+	public PylonM158() {
+	}
+
+	static {
+		Property.set(PylonM158.class, "mesh", "3DO/Arms/M158/mono.sim");
+	}
+}
