1c4ad8bdba7aac7514c91d5bb912df8568674a12
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2014-06-09 09:46:10 +0000

    Initial AH-1G commit

---
+++ AH-1G/src/com/maddox/il2/objects/weapons/PylonCobra.java
@@ -0,0 +1,13 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class PylonCobra extends PylonRO_82_1 {
+
+	public PylonCobra() {
+	}
+
+	static {
+		Property.set(PylonCobra.class, "mesh", "3DO/Arms/cobrapylon/mono.sim");
+	}
+}
