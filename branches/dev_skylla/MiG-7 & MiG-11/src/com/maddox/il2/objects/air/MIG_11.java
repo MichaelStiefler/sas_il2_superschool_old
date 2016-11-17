11d8002fb4b14bd3187bf86033f9ccd09b14537c
skylla <skylla@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2016-11-17 19:22:05 +0000

    a small update .. incremental steps, nice and slow ..

--- branches/dev_skylla/MiG-7 & MiG-11/src/com/maddox/il2/objects/air/MIG_11.java
+++ branches/dev_skylla/MiG-7 & MiG-11/src/com/maddox/il2/objects/air/MIG_11.java
@@ -9,9 +9,7 @@ import com.maddox.sas1946.il2.util.AircraftTools;
 
 /**
  * @author SAS~Skylla, SAS~Storebror
- * @see CockpitMIG_7.java
- * 
- * Momentan nur das Gerüst, zu mehr keine Zeit heute .. 
+ * @see CockpitMIG_7.java, MIG_7.java
  * 
  * air.ini: 
  * 	MiG-11            air.MIG_11 1                          NOINFO  r01   SUMMER
@@ -27,27 +25,13 @@ import com.maddox.sas1946.il2.util.AircraftTools;
  *	#####################################################################
  *	MIG-11.default         Default (2x 20mm ShVak)
  *	MIG-11.none            None
- * 
- * Ich würde die (Grund)Entwicklung in UP3 bevorzugen, 4.12.2, 4.09 danach.
- * skin1o.tgb passt für uns Clowns natürlich nicht, muss auf 512x512 verkleinert werden.
- * 
- * -----------------------------------------------------------------------------------------
- * 
- * TODO ([-] ausstehend, [x] erledigt):
- * 
- * 	> [-] Cockpitinstrumente 
- * 	> [-] move Cockpit Door
- * 	> [-] Fahrwerkanimation korrigieren
- * 	> [-] Loadouts in cod Format
- * 	> [-] FlightModel
- * 
- * Die Liste wird noch auf übersehene Baustellen erweitert!
- * 
- * 
+ *
  * ------------------------------------------------------------------------------------------
 **/
 
-public class MIG_11 extends MIG_3 {
+//TODO wär's nicht sinnvoller hier einfach extends MIG_7 zu schreiben? 
+public class MIG_11 extends MIG_7 {
+/*
     
     public static void moveGear(HierMesh hiermesh, float f, float f1, float f2)
     {
@@ -85,25 +69,18 @@ public class MIG_11 extends MIG_3 {
             setDoorSnd(f);
         }
     }
-
+*/
     static {
 	        final Class aircraftClass = MIG_11.class;
 	        new SPAWN(aircraftClass);
 	        Property.set(aircraftClass, "iconFar_shortClassName", "MiG-11");
 	        Property.set(aircraftClass, "meshName", "3DO/Plane/MiG-11/hier.him");
 	        Property.set(aircraftClass, "PaintScheme", new PaintSchemeFMPar01());
-	        //fictional, also 1946 ..
-	        //strike out the above, it's not fictional.
-	        //this is the Mikojan-Gurewitsch I-224, maiden flight was on 20 october 1944
 	        Property.set(aircraftClass, "yearService", 1944.0f);
-	        //wäre in Korea noch verwendet worden .. 
-	        Property.set(aircraftClass, "yearExpired", 1953.5f); 
+	        Property.set(aircraftClass, "yearExpired", 1953.5f);
 	        Property.set(aircraftClass, "FlightModel", "FlightModels/MiG-3ud.fmd");
 	        Property.set(aircraftClass, "cockpitClass", new Class[] { CockpitMIG_7.class });
-	        //TODO: was macht denn dieser Wert?
-            //DONE: Das ist die "Line Of Sight Elevation", die Angabe (in Meter), wie hoch Du über die Geschossbahn blickst.
 	        Property.set(aircraftClass, "LOSElevation", 0.906f);
-	        //was soll die Krebserei mit Trigger 1? Es gibt nur eine Waffenart zum feuern, also Trigger 0!
 	        Aircraft.weaponTriggersRegister(aircraftClass, new int[] { 0, 0 });
 	        Aircraft.weaponHooksRegister(aircraftClass, new String[] { "_MGUN01", "_MGUN02" });
 	        AircraftTools.weaponsRegister(aircraftClass, "default", new String[] { "MGunShVAKs 100", "MGunShVAKs 100" });
