df9415fda62f93d7ac998c82a433da8d787f1098
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2016-12-13 16:14:08 +0000

    update Weapon pack Gen2016 - F-5 fueltanks , SkinnableOrdnance

---
+++ branches/dev_western/Weapon_Generation2016/src/com/maddox/il2/objects/weapons/SkinnableOrdnance.java
@@ -0,0 +1,14 @@
+// Source File Name: SkinnableOrdnance
+// Author:           western0221 and SAS~Storebror
+// Last Modified by: western0221 2016-12-12
+
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.il2.engine.Mat;
+
+
+public interface SkinnableOrdnance
+{
+    public abstract void matPlane(Mat mat, Mat matp);
+}
+
