cbbe97544cbf4442bfc1796fbb7f2ee42a6ee172
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-01-05 02:29:07 +0000

    Update Weapon pack Generation 2016 add - Mk81SnakeEye, Mk83 and Mk84 Ballute, FAB1000M43 change - Mk81 (LDGP) spec, Mk77 and Mk79 use BombNapalmGeneric

---
+++ Weapon_Generation2016/src/com/maddox/il2/objects/weapons/BombMk84Ballute_gn16.java
@@ -0,0 +1,57 @@
+
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.il2.ai.RangeRandom;
+import com.maddox.il2.ai.World;
+import com.maddox.il2.engine.ActorMesh;
+import com.maddox.rts.Property;
+
+
+public class BombMk84Ballute_gn16 extends Bomb
+{
+
+    public BombMk84Ballute_gn16()
+    {
+        bBalDeployed = false;
+    }
+
+    protected boolean haveSound()
+    {
+        return false;
+    }
+
+    public void start()
+    {
+        super.start();
+        ttcurTM = World.Rnd().nextFloat(0.5F, 1.0F);
+    }
+
+    public void interpolateTick()
+    {
+        super.interpolateTick();
+        if(!bBalDeployed && super.curTm > ttcurTM)
+        {
+            bBalDeployed = true;
+            super.S *= 120F;
+            setMesh("3DO/Arms/Mk84_gn16/monobal_dep.sim");
+        }
+    }
+
+    private boolean bBalDeployed;
+    private float ttcurTM;
+
+    static 
+    {
+        Class class1 = com.maddox.il2.objects.weapons.BombMk84Ballute_gn16.class;
+        Property.set(class1, "mesh", "3DO/Arms/Mk84_gn16/monobal.sim");
+        Property.set(class1, "radius", 400.65F);
+        Property.set(class1, "power", 428.644F);
+        Property.set(class1, "powerType", 0);
+        Property.set(class1, "kalibr", 0.457F);
+        Property.set(class1, "massa", 893.57F);
+        Property.set(class1, "sound", "weapon.bomb_big");
+        Property.set(class1, "fuze", ((Object) (new Object[] {
+            com.maddox.il2.objects.weapons.Fuze_AN_M100.class, com.maddox.il2.objects.weapons.Fuze_M115.class, com.maddox.il2.objects.weapons.Fuze_M112.class
+        })));
+    }
+}
