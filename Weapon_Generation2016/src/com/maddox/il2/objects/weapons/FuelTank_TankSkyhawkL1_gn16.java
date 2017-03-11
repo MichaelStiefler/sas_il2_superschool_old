c9093bed53bf6992630fe4b44bbd12121898e73a
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-03-11 15:51:22 +0000

    Weapon pack Generation 2016 , add new classes

---
+++ Weapon_Generation2016/src/com/maddox/il2/objects/weapons/FuelTank_TankSkyhawkL1_gn16.java
@@ -0,0 +1,57 @@
+// 300 gal. Fueltank for A-4 Skyhawk and A-6 Intruder , AV-8B Harrier with 1x left tailfin
+
+/*
+* Base color is low visibility dark gray for 1980s and later.
+
+* When you want high visibility white gray for 1960-1970s, add this code to mother Jets.
+
+    public void missionStarting()
+    {
+        super.missionStarting();
+        checkChangeWeaponColors();
+    }
+
+    private void checkChangeWeaponColors()
+    {
+        for(int i = 0; i < FM.CT.Weapons.length; i++)
+            if(FM.CT.Weapons[i] != null)
+            {
+                for(int j = 0; j < FM.CT.Weapons[i].length; j++)
+                {
+                    if(FM.CT.Weapons[i][j] instanceof FuelTankGun_TankSkyhawkL1_gn16)
+                        ((FuelTankGun_TankSkyhawkL1_gn16)FM.CT.Weapons[i][j]).matHighvis();
+                }
+            }
+    }
+
+*/
+
+
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+
+public class FuelTank_TankSkyhawkL1_gn16 extends FuelTank
+{
+
+    public FuelTank_TankSkyhawkL1_gn16()
+    {
+    }
+
+    public void matHighvis()
+    {
+        setMesh(Property.stringValue(getClass(), "mesh"));
+        mesh.materialReplace("Tank_Gloss", "Tank_GlossHV");
+        mesh.materialReplace("Tank_GlossP", "Tank_GlossHVP");
+        mesh.materialReplace("Tank_GlossQ", "Tank_GlossHVQ");
+    }
+
+    static 
+    {
+        Class class1 = com.maddox.il2.objects.weapons.FuelTank_TankSkyhawkL1_gn16.class;
+        Property.set(class1, "mesh", "3DO/Arms/TankSkyhawk_gn16/monol1.sim");
+        Property.set(class1, "kalibr", 0.6F);
+        Property.set(class1, "massa", 1150F);
+    }
+}
