d4c97b43082e5435f3c93127f9cb2253679aa9d8
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-04-05 00:53:02 +0000

    add Weapon pack Generation 2016 -- F-100 fueltanks, F-105 fueltanks, QRC-160 ECM pod, GAU-8 mgun class

---
+++ branches/dev_western/Weapon_Generation2016/src/com/maddox/il2/objects/weapons/FuelTank_TankF105_450gal_gn16.java
@@ -0,0 +1,23 @@
+
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+// Referenced classes of package com.maddox.il2.objects.weapons:
+//            FuelTank
+
+public class FuelTank_TankF105_450gal_gn16 extends FuelTank
+{
+
+    public FuelTank_TankF105_450gal_gn16()
+    {
+    }
+
+    static 
+    {
+        Class class1 = com.maddox.il2.objects.weapons.FuelTank_TankF105_450gal_gn16.class;
+        Property.set(class1, "mesh", "3DO/Arms/TankF105_450gal_gn16/mono.sim");
+        Property.set(class1, "kalibr", 1.5F);
+        Property.set(class1, "massa", 1550F);
+    }
+}
