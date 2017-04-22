903b996bb68b97ef03522ce667a8a6d9f2139242
skylla <skylla@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-04-22 11:11:07 +0000

    reworked net-replication, introduced an abstract class for the alterable Miniguns

--- AC-47_rework/src/com/maddox/il2/objects/weapons/MGunMiniGun6000.java
+++ AC-47_rework/src/com/maddox/il2/objects/weapons/MGunMiniGun6000.java
@@ -8,37 +8,17 @@ import com.maddox.il2.engine.GunProperties;
 
 /**
  * This class was modified by SAS~Skylla in the course of the AC-47 rework.
- * @see AC_47, MGunMiniGun3000
+ * @see AC_47, MGunAdjustableMiniGun, MGunMiniGun3000
 **/
 
-public class MGunMiniGun6000 extends MGunMiniGun3000 {
+public class MGunMiniGun6000 extends MGunAdjustableMiniGun {
 	
 	public MGunMiniGun6000() {
 		rpm = 6000F;
+		maxRPM = 6000F;
+		minRPM = 3000F;
+		stepRPM = 3000F;
 	}
-	
-  //Alterable RPM: -------------------------------------------------------------------------------------
-	
-	public float incRPM() {
-		if(rpm + 3000F <= 6000F) {
-			rpm += 3000F;
-		}
-		return rpm;
-	}
-	
-	public float decRPM() {
-		if(rpm - 3000F >= 3000F) {
-			rpm -= 3000F;
-		}
-		return rpm;
-	}
-	
-	public float resRPM() {
-		this.rpm = 6000F;
-		return rpm;
-	}
-	
-  //----------------------------------------------------------------------------------------------------
 	
     public GunProperties createProperties() {
         final GunProperties properties = super.createProperties();
