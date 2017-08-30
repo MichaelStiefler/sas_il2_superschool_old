b3979e4d502a68ee4135f64e90dcbe97ee89a997
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-08-30 15:37:38 +0000

    misleaded commit undone

--- global/globals.h
+++
@@ -1,6 +0,0 @@
-#define G_FILE_VERSION "3.2.0.0 Build 17052201"
-#define G_PRODUCT_VERSION "3.2.0"
-#define G_FILEVERSION 3,2,0,0
-#define G_PRODUCTVERSION 3,2,0,0
-#define G_SPECIAL_BUILD "Visual Studio 2017"
-#define G_COPYRIGHT "(C) SAS May 2017"
