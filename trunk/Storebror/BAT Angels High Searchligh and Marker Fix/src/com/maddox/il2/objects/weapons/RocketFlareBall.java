fceb17269fdb1e44e7198eaf42ba24461394dde2
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-11-01 16:45:56 +0000

    Searchlight and Marker Fix for BAT WIP 2017-11-01

--- trunk/Storebror/BAT Angels High Searchligh and Marker Fix/src/com/maddox/il2/objects/weapons/RocketFlareBall.java
+++ trunk/Storebror/BAT Angels High Searchligh and Marker Fix/src/com/maddox/il2/objects/weapons/RocketFlareBall.java
@@ -16,136 +16,144 @@ import com.maddox.rts.Property;
 import com.maddox.rts.Time;
 import com.maddox.sound.SoundFX;
 
-public class RocketFlareBall extends Rocket
-{
+public class RocketFlareBall extends Rocket {
 
-    public boolean interpolateStep()
-    {
+    public boolean interpolateStep() {
         // Randomize Light Emission ("Flicker" Flare), take current Flare Size into account.
         float randomFactor = World.rnd().nextFloat();
-        float timeFactor = 1.0F - Math.abs((float)(Time.current() - this.timeStart) / (float)(RADIUS_PERIOD / 2) - 1.0F);
-        if (timeFactor < 0F) timeFactor = 0F;
-        float endFactor = MiscEffects.cvt((float)(this.timeStart + this.timeLife - Time.current()), 0F, 1000F, 0F, 1F);
-        Eff3DActor.setIntesity(eff3dactor, (randomFactor * 4.75F + 0.25F) * endFactor);
-        if (this.lightpointactor != null) {
-            lightPointIntensity = (randomFactor + 0.5F) * timeFactor * endFactor;
-            this.lightpointactor.light.setEmit(lightPointIntensity, lightPointRange);
+        float timeFactor = 1.0F - Math.abs(((float) (Time.current() - this.timeStart) / (float) (RADIUS_PERIOD / 2)) - 1.0F);
+        if (timeFactor < 0F) {
+            timeFactor = 0F;
         }
+        float endFactor = MiscEffects.cvt((this.timeStart + this.timeLife) - Time.current(), 0F, 1000F, 0F, 1F);
+        Eff3DActor.setIntesity(this.eff3dactor, ((randomFactor * 4.75F) + 0.25F) * endFactor);
+        if (this.lightpointactor != null) {
+            this.lightPointIntensity = (randomFactor + 0.5F) * timeFactor * endFactor;
+            this.lightpointactor.light.setEmit(this.lightPointIntensity, this.lightPointRange);
+        }
+//        this.light.light.setEmit(this.lightPointIntensity, this.lightPointRange);
         this.getSpeed(this.v);
         if (this.v.z < -15D) {
             this.v.z *= 0.9D;
             this.setSpeed(this.v);
         }
-        
+
         // Relocate Flare Sound emitter position
-        if (draw != null && draw.sounds() != null) {
-            SoundFX soundfx = draw.sounds().get();
+        if ((this.draw != null) && (this.draw.sounds() != null)) {
+            SoundFX soundfx = this.draw.sounds().get();
             soundfx.setPosition(this.pos.getAbsPoint());
         }
         return super.interpolateStep();
     }
 
-    public RocketFlareBall(Actor actor, int i, Point3d point3d, Orient orient, float f)
-    {
+    public RocketFlareBall(Actor actor, int i, Point3d point3d, Orient orient, float f) {
         this(actor, point3d, orient);
     }
 
-    public RocketFlareBall(Actor actor, Point3d point3d, Orient orient)
-    {
-        speed = new Vector3d();
-        pos.setAbs(point3d, orient);
-        pos.reset();
-        pos.setBase(actor, (Hook)null, true);
-        collide(false);
-        drawing(false);
-        eff3dactor = null;
-        lightpointactor = null;
-        lightPointIntensity = 0F;
-        lightPointRange = 0F;
+    public RocketFlareBall(Actor actor, Point3d point3d, Orient orient) {
+        this.speed = new Vector3d();
+        this.pos.setAbs(point3d, orient);
+        this.pos.reset();
+        this.pos.setBase(actor, (Hook) null, true);
+        this.collide(false);
+        this.drawing(false);
+        this.eff3dactor = null;
+        this.lightpointactor = null;
+        this.lightPointIntensity = 0F;
+        this.lightPointRange = 0F;
     }
 
-    public void start(float f, int i)
-    {
+    public void start(float f, int i) {
 //        System.out.println("RocketFlareBall start(" + f + ", " + i + ")");
         super.start(f, i);
-        speed.normalize();
-        speed.scale(110D);
-        drawing(false);
-        eff3dactor = Eff3DActor.New(this, null, new Loc(), 1.0F, "3DO/Effects/Fireworks/FlareBall.eff", f);
+        this.speed.normalize();
+        this.speed.scale(110D);
+        this.drawing(false);
+        String flareEffect = Property.stringValue(this.getClass(), "effect");
+        System.out.println("RocketFlareBall flareEffect=" + flareEffect);
+        this.eff3dactor = Eff3DActor.New(this, null, new Loc(), 1.0F, flareEffect, f);
+//        this.eff3dactor = Eff3DActor.New(this, null, new Loc(), 1.0F, "3DO/Effects/Fireworks/FlareBall.eff", f);
         this.timeStart = Time.current();
-        this.timeLife = (long)(f*1000F);
-        
+        this.timeLife = (long) (f * 1000F);
+
         // Create Light Emission according to current darkness level
         if (World.Sun().ToSun.z < 0.1F) {
-            lightPointIntensity = 0.5F + World.rnd().nextFloat();
-            lightPointRange = MiscEffects.cvt(World.Sun().ToSun.z, -0.3F, 0.1F, 1000F, 100F);
-            lightpointactor = new LightPointActor(new LightPointWorld(), new Point3d());
-            lightpointactor.light.setColor(1.0F, 0.9F, 0.6F);
-            lightpointactor.light.setEmit(lightPointIntensity, lightPointRange);
-            ((Actor) (eff3dactor)).draw.lightMap().put("light", lightpointactor);
+            this.lightPointIntensity = 0.5F + World.rnd().nextFloat();
+            this.lightPointRange = MiscEffects.cvt(World.Sun().ToSun.z, -0.3F, 0.1F, 1000F, 100F);
+            this.lightpointactor = new LightPointActor(new LightPointWorld(), new Point3d());
+            Color3f lightColor = (Color3f)Property.value(this.getClass(), "lightColor");
+            System.out.println("RocketFlareBall Color=" + lightColor.x + " : " + lightColor.y + " : " + lightColor.z);
+            this.lightpointactor.light.setColor(lightColor);
+//            this.lightpointactor.light.setColor(1.0F, 0.9F, 0.6F);
+            this.lightpointactor.light.setEmit(this.lightPointIntensity, this.lightPointRange);
+            ((Actor) (this.eff3dactor)).draw.lightMap().put("light", this.lightpointactor);
         }
-        
+
         // Shoot Flare Gun, play sound
-        SoundFX soundfx = newSound("weapon.rocketflare", true);
-        if(soundfx.isInitialized()) {
-            soundfx.setAcoustics(acoustics);
-            if (draw != null && draw.sounds() != null)
-                soundfx.insert(draw.sounds(), true);
+        SoundFX soundfx = this.newSound("weapon.rocketflare", true);
+        if (soundfx.isInitialized()) {
+            soundfx.setAcoustics(this.acoustics);
+            if ((this.draw != null) && (this.draw.sounds() != null)) {
+                soundfx.insert(this.draw.sounds(), true);
+            }
             soundfx.play(this.pos.getAbsPoint());
 //            System.out.println("soundfx=" + soundfx.hashCode());
         }
     }
 
-    protected void doExplosion(Actor actor, String s)
-    {
-        destroy();
+    protected void doExplosion(Actor actor, String s) {
+        this.destroy();
     }
 
-    protected void doExplosionAir()
-    {
+    protected void doExplosionAir() {
     }
-    
+
     public void destroy() {
 //        System.out.println("RocketFlareBall destroy()");
-        
+
         // Turn off flare illumination if there is any
         if (this.lightpointactor != null) {
             this.lightpointactor.destroy();
         }
-        
+
         // Stop Flare Sound
         this.stopSounds();
-        
+
         super.destroy();
     }
 
-    private Vector3d v = new Vector3d();
-    private Eff3DActor eff3dactor;
-    private LightPointActor lightpointactor;
-    private float lightPointIntensity;
-    private float lightPointRange;
-    private long timeStart;
+    private Vector3d          v             = new Vector3d();
+    private Eff3DActor        eff3dactor;
+    private LightPointActor   lightpointactor;
+    private float             lightPointIntensity;
+    private float             lightPointRange;
+    private long              timeStart;
     private static final long RADIUS_PERIOD = 25000L;
+    
+    static void initCommon(Class theClass) {
+        Property.set(theClass, "smoke", "3do/effects/rocket/RocketSmokeWhiteTile.eff");
+        Property.set(theClass, "sprite", (Object) null);
+        Property.set(theClass, "flame", (Object) null);
+        Property.set(theClass, "emitLen", 1000.0F);
+        Property.set(theClass, "emitMax", 1.0F);
+        Property.set(theClass, "sound", (Object) null); // Don't set the sound preset here, it won't work.
+        Property.set(theClass, "radius", 0.1F);
+        Property.set(theClass, "timeLife", 40F);
+        Property.set(theClass, "timeFire", 10F);
+        Property.set(theClass, "force", 0.0F);
+        Property.set(theClass, "power", 0.01485F);
+        Property.set(theClass, "powerType", 0);
+        Property.set(theClass, "kalibr", 0.001F);
+        Property.set(theClass, "massa", 3F);
+        Property.set(theClass, "massaEnd", 0.1F);
+    }
 
-    static 
-    {
-        Class class1 = RocketFlareBall.class;
-        Property.set(class1, "mesh", "3DO/Arms/RocketFlareBall/mono.sim");
-        Property.set(class1, "sprite", (Object)null);
-        Property.set(class1, "flame", (Object)null);
-        Property.set(class1, "smoke", "3do/effects/rocket/RocketSmokeWhiteTile.eff");
-        Property.set(class1, "emitColor", new Color3f(0.8F, 0.8F, 1.0F));
-        Property.set(class1, "emitLen", 0.0F);
-        Property.set(class1, "emitMax", 0.0F);
-        Property.set(class1, "sound", (Object)null); // Don't set the sound preset here, it won't work.
-        Property.set(class1, "radius", 0.1F);
-        Property.set(class1, "timeLife", 40F);
-        Property.set(class1, "timeFire", 10F);
-        Property.set(class1, "force", 0.0F);
-        Property.set(class1, "power", 0.01485F);
-        Property.set(class1, "powerType", 0);
-        Property.set(class1, "kalibr", 0.001F);
-        Property.set(class1, "massa", 3F);
-        Property.set(class1, "massaEnd", 0.1F);
+    static {
+        Class theClass = RocketFlareBall.class;
+        initCommon(theClass);
+        Property.set(theClass, "mesh", "3DO/Arms/RocketFlareBall/mono.sim");
+        Property.set(theClass, "effect", "3DO/Effects/Fireworks/FlareBallWhite.eff");
+        Property.set(theClass, "lightColor", new Color3f(1.0F, 0.9F, 0.6F));
+        Property.set(theClass, "emitColor", new Color3f(0.8F, 0.8F, 1.0F));
     }
 }
