4ce8a1d6855ca3d00c5e19163e4b7a507a8f8a6f
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-11-01 16:45:18 +0000

    C&C mod for BAT WIP 2017-11-01

--- trunk/Storebror/BAT Angels High Command and Control Fix/src/com/maddox/il2/objects/vehicles/stationary/CandCGeneric.java
+++ trunk/Storebror/BAT Angels High Command and Control Fix/src/com/maddox/il2/objects/vehicles/stationary/CandCGeneric.java
@@ -1,7 +1,6 @@
 package com.maddox.il2.objects.vehicles.stationary;
 
 import java.io.IOException;
-import java.util.Random;
 
 import com.maddox.JGP.Point3d;
 import com.maddox.JGP.Vector3d;
@@ -24,90 +23,70 @@ import com.maddox.rts.Property;
 import com.maddox.rts.Time;
 import com.maddox.sound.SoundFX;
 
-public abstract class CandCGeneric extends ActorHMesh
-    implements ActorAlign
-{
-    class Move extends Interpolate
-    {
+public abstract class CandCGeneric extends ActorHMesh implements ActorAlign {
+    class Move extends Interpolate {
 
-        public boolean tick()
-        {
-            if(engineSFX != null)
-                if(engineSTimer >= 0)
-                {
-                    if(--engineSTimer <= 0)
-                    {
-                        engineSTimer = (int)CandCGeneric.SecsToTicks(CandCGeneric.Rnd(Timer1, Timer2));
-                        if(!danger())
-                            engineSTimer = -engineSTimer;
+        public boolean tick() {
+            if (CandCGeneric.this.engineSFX != null) {
+                if (CandCGeneric.this.engineSTimer >= 0) {
+                    if (--CandCGeneric.this.engineSTimer <= 0) {
+                        CandCGeneric.this.engineSTimer = (int) CandCGeneric.SecsToTicks(CandCGeneric.Rnd(CandCGeneric.this.Timer1, CandCGeneric.this.Timer2));
+                        if (!CandCGeneric.this.danger()) {
+                            CandCGeneric.this.engineSTimer = -CandCGeneric.this.engineSTimer;
+                        }
                     }
-                } else
-                if(++engineSTimer >= 0)
-                {
-                    engineSTimer = -(int)CandCGeneric.SecsToTicks(CandCGeneric.Rnd(Timer1, Timer2));
-                    if(danger())
-                        engineSTimer = -engineSTimer;
+                } else if (++CandCGeneric.this.engineSTimer >= 0) {
+                    CandCGeneric.this.engineSTimer = -(int) CandCGeneric.SecsToTicks(CandCGeneric.Rnd(CandCGeneric.this.Timer1, CandCGeneric.this.Timer2));
+                    if (CandCGeneric.this.danger()) {
+                        CandCGeneric.this.engineSTimer = -CandCGeneric.this.engineSTimer;
+                    }
                 }
+            }
             return true;
         }
 
-        Move()
-        {
+        Move() {
         }
     }
 
-    class Master extends ActorNet
-    {
+    class Master extends ActorNet {
 
-        public boolean netInput(NetMsgInput netmsginput)
-            throws IOException
-        {
+        public boolean netInput(NetMsgInput netmsginput) throws IOException {
             return true;
         }
 
-        public Master(Actor actor)
-        {
+        public Master(Actor actor) {
             super(actor);
         }
     }
 
-    class Mirror extends ActorNet
-    {
+    class Mirror extends ActorNet {
 
-        public boolean netInput(NetMsgInput netmsginput)
-            throws IOException
-        {
+        public boolean netInput(NetMsgInput netmsginput) throws IOException {
             return true;
         }
 
         NetMsgFiltered out;
 
-        public Mirror(Actor actor, NetChannel netchannel, int i)
-        {
+        public Mirror(Actor actor, NetChannel netchannel, int i) {
             super(actor, netchannel, i);
-            out = new NetMsgFiltered();
+            this.out = new NetMsgFiltered();
         }
     }
 
-    public static class SPAWN
-        implements ActorSpawn
-    {
+    public static class SPAWN implements ActorSpawn {
 
-        public Actor actorSpawn(ActorSpawnArg actorspawnarg)
-        {
+        public Actor actorSpawn(ActorSpawnArg actorspawnarg) {
             CandCGeneric candcgeneric = null;
-            try
-            {
+            try {
                 CandCGeneric.constr_arg2 = actorspawnarg;
-                candcgeneric = (CandCGeneric)cls.newInstance();
+                candcgeneric = (CandCGeneric) this.cls.newInstance();
                 CandCGeneric.constr_arg2 = null;
-            }
-            catch(Exception exception)
-            {
+            } catch (Exception exception) {
                 CandCGeneric.constr_arg2 = null;
                 System.out.println(exception.getMessage());
                 exception.printStackTrace();
-                System.out.println("SPAWN: Can't create CandCGeneric object [class:" + cls.getName() + "]");
+                System.out.println("SPAWN: Can't create CandCGeneric object [class:" + this.cls.getName() + "]");
                 return null;
             }
             return candcgeneric;
@@ -115,155 +94,133 @@ public abstract class CandCGeneric extends ActorHMesh
 
         public Class cls;
 
-        public SPAWN(Class class1)
-        {
+        public SPAWN(Class class1) {
             Property.set(class1, "iconName", "icons/unknown.mat");
             Property.set(class1, "meshName", CandCGeneric.mesh_name);
-            cls = class1;
-            com.maddox.rts.Spawn.add(cls, this);
+            this.cls = class1;
+            com.maddox.rts.Spawn.add(this.cls, this);
         }
     }
 
-
-    public void setTimer(int i)
-    {
-        Random random = new Random();
-        Timer1 = (float)((double)random.nextInt(i) * 0.10000000000000001D);
-        Timer2 = (float)((double)random.nextInt(i) * 0.10000000000000001D);
+    public void setTimer(int i) {
+//        Random random = new Random(); // TODO: Fixed by SAS~Storebror: Don't create new Random Classes all the time!!!
+        this.Timer1 = (float) (World.Rnd().nextInt(i) * 0.1D); // TODO: Fixed by SAS~Storebror: Don't create new Random Classes all the time!!!
+        this.Timer2 = (float) (World.Rnd().nextInt(i) * 0.1D); // TODO: Fixed by SAS~Storebror: Don't create new Random Classes all the time!!!
     }
 
-    public void resetTimer(float f)
-    {
-        Timer1 = f;
-        Timer2 = f;
+    public void resetTimer(float f) {
+        this.Timer1 = f;
+        this.Timer2 = f;
     }
 
-    public static double Rnd(double d, double d1)
-    {
+    public static double Rnd(double d, double d1) {
         return World.Rnd().nextDouble(d, d1);
     }
 
-    public static float Rnd(float f, float f1)
-    {
+    public static float Rnd(float f, float f1) {
         return World.Rnd().nextFloat(f, f1);
     }
 
-    public boolean RndB(float f)
-    {
+    public boolean RndB(float f) {
         return World.Rnd().nextFloat(0.0F, 1.0F) < f;
     }
 
-    public static long SecsToTicks(float f)
-    {
-        long l = (long)(0.5D + (double)(f / Time.tickLenFs()));
+    public static long SecsToTicks(float f) {
+        long l = (long) (0.5D + f / Time.tickLenFs());
         return l < 1L ? 1L : l;
     }
 
-    public void destroy()
-    {
-        if(isDestroyed())
-        {
+    public void destroy() {
+        if (this.isDestroyed()) {
             return;
-        } else
-        {
-            engineSFX = null;
-            engineSTimer = 0xfa0a1f01;
+        } else {
+            this.engineSFX = null;
+            this.engineSTimer = 0xfa0a1f01; // funny value :)
             super.destroy();
             return;
         }
     }
 
-    public Object getSwitchListener(Message message)
-    {
+    public Object getSwitchListener(Message message) {
         return this;
     }
 
-    public boolean isStaticPos()
-    {
+    public boolean isStaticPos() {
         return true;
     }
 
-    public CandCGeneric()
-    {
+    public CandCGeneric() {
         this(constr_arg2);
     }
 
-    public CandCGeneric(ActorSpawnArg actorspawnarg)
-    {
+    public CandCGeneric(ActorSpawnArg actorspawnarg) {
         mesh_name = "3do/primitive/siren/mono.sim";
-        engineSFX = null;
-        engineSTimer = 0x98967f;
-        outCommand = new NetMsgFiltered();
+        this.engineSFX = null;
+        this.engineSTimer = 9999999;
+        this.outCommand = new NetMsgFiltered();
         actorspawnarg.setStationary(this);
-        myArmy = getArmy();
-        setArmy(getArmy());
-        collide(false);
-        drawing(true);
-        createNetObject(actorspawnarg.netChannel, actorspawnarg.netIdRemote);
-        heightAboveLandSurface = 0.0F;
-        Align();
-        startMove();
-        Timer1 = 30F;
-        Timer2 = 50F;
-        delay = 3F;
+        this.myArmy = this.getArmy();
+        this.setArmy(this.getArmy());
+        this.collide(false);
+        this.drawing(true);
+        this.createNetObject(actorspawnarg.netChannel, actorspawnarg.netIdRemote);
+        this.heightAboveLandSurface = 0.0F;
+        this.Align();
+        this.startMove();
+        this.Timer1 = 30F;
+        this.Timer2 = 50F;
+        this.delay = 3F;
     }
 
-    public void startMove()
-    {
-        if(!interpEnd("move"))
-        {
-            interpPut(new Move(), "move", Time.current(), null);
-            engineSFX = newSound("objects.siren", false);
-            engineSTimer = -(int)SecsToTicks(Rnd(delay, delay));
+    public void startMove() {
+        if (!this.interpEnd("move")) {
+            this.interpPut(new Move(), "move", Time.current(), null);
+            this.engineSFX = this.newSound("objects.siren", false);
+            this.engineSTimer = -(int) SecsToTicks(Rnd(this.delay, this.delay));
         }
     }
 
-    public void Align()
-    {
-        pos.getAbs(p);
-        p.z = Engine.land().HQ(p.x, p.y) + (double)heightAboveLandSurface;
-        o.setYPR(pos.getAbsOrient().getYaw(), 0.0F, 0.0F);
+    public void Align() {
+        this.pos.getAbs(p);
+        p.z = Engine.land().HQ(p.x, p.y) + this.heightAboveLandSurface;
+        o.setYPR(this.pos.getAbsOrient().getYaw(), 0.0F, 0.0F);
         Engine.land().N(p.x, p.y, n);
         o.orient(n);
-        pos.setAbs(p, o);
+        this.pos.setAbs(p, o);
     }
 
-    public void align()
-    {
-        Align();
+    public void align() {
+        this.Align();
     }
 
-    public boolean danger()
-    {
+    public boolean danger() {
         return false;
     }
 
-    public void createNetObject(NetChannel netchannel, int i)
-    {
-        if(netchannel == null)
-            net = new Master(this);
-        else
-            net = new Mirror(this, netchannel, i);
+    public void createNetObject(NetChannel netchannel, int i) {
+        if (netchannel == null) {
+            this.net = new Master(this);
+        } else {
+            this.net = new Mirror(this, netchannel, i);
+        }
     }
 
-    public void netFirstUpdate(NetChannel netchannel)
-        throws IOException
-    {
+    public void netFirstUpdate(NetChannel netchannel) throws IOException {
     }
 
-    public static String mesh_name;
-    public float heightAboveLandSurface;
-    public SoundFX engineSFX;
-    public int engineSTimer;
-    public int myArmy;
+    public static String        mesh_name;
+    public float                heightAboveLandSurface;
+    public SoundFX              engineSFX;
+    public int                  engineSTimer;
+    public int                  myArmy;
     public static ActorSpawnArg constr_arg2 = null;
-    public static Point3d p = new Point3d();
-    public static Orient o = new Orient();
-    public static Vector3f n = new Vector3f();
-    public static Vector3d tmpv = new Vector3d();
-    public NetMsgFiltered outCommand;
-    public float Timer1;
-    public float Timer2;
-    public float delay;
+    public static Point3d       p           = new Point3d();
+    public static Orient        o           = new Orient();
+    public static Vector3f      n           = new Vector3f();
+    public static Vector3d      tmpv        = new Vector3d();
+    public NetMsgFiltered       outCommand;
+    public float                Timer1;
+    public float                Timer2;
+    public float                delay;
 
 }
