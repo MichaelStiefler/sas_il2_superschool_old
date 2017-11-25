cf4b8ac96e0e37f3398b434a3b9e0aafba2aa0a4
Michael Stiefler <michael.stiefler@gmail.com>
2017-11-25 09:00:16 +0000

    Service Pack 3 Fuze Patch

--- BAT/Angels High/Service Pack 2/BAT Angels High Service Pack 2 WAW/src/com/maddox/il2/objects/weapons/Bomb500lbsE_MC.java
+++ BAT/Angels High/Service Pack 2/BAT Angels High Service Pack 2 WAW/src/com/maddox/il2/objects/weapons/Bomb500lbsE_MC.java
@@ -12,5 +12,13 @@ public class Bomb500lbsE_MC extends Bomb {
         Property.set(class1, "kalibr", 0.327F);
         Property.set(class1, "massa", 235.868F);
         Property.set(class1, "sound", "weapon.bomb_big");
+        try
+        {
+          Class fuze1 = Class.forName("com.maddox.il2.objects.weapons.Fuze_PistolNo44");
+          Class fuze2 = Class.forName("com.maddox.il2.objects.weapons.Fuze_PistolNo54");
+          Class fuze3 = Class.forName("com.maddox.il2.objects.weapons.Fuze_PistolNo28");
+          Property.set(class1, "fuze", new Object[] { fuze1, fuze2, fuze3 });
+        }
+        catch (Exception localException) {}
     }
 }
