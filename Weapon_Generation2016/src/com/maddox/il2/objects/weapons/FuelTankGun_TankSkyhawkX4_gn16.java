faa13e79bb33471c44d5e25b2cc33f84f5c085ac
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-12-06 19:47:32 +0000

    Update Weapon pack Generation 2016 --- add 300gal. Tank Skyhawk X4 tailfin variant for S-3 Viking

---
+++ Weapon_Generation2016/src/com/maddox/il2/objects/weapons/FuelTankGun_TankSkyhawkX4_gn16.java
@@ -0,0 +1,60 @@
+// 300 gal. Fueltank for S-3 Viking with 4x X style tailfin
+
+/*
+* Base color is low visibility dark gray for 1980s and later.
+
+* When you want high visibility white gray for 1960-1970s, add this code to mother Jets.
+
+    public void missionStarting()
+    {
+        super.missionStarting();
+        checkChangeWeaponColors();
+    }
+
+    private void checkChangeWeaponColors()
+    {
+        for(int i = 0; i < FM.CT.Weapons.length; i++)
+            if(FM.CT.Weapons[i] != null)
+            {
+                for(int j = 0; j < FM.CT.Weapons[i].length; j++)
+                {
+                    if(FM.CT.Weapons[i][j] instanceof FuelTankGun_TankSkyhawkX4_gn16)
+                        ((FuelTankGun_TankSkyhawkX4_gn16)FM.CT.Weapons[i][j]).matHighvis();
+                }
+            }
+    }
+
+*/
+
+
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+
+public class FuelTankGun_TankSkyhawkX4_gn16 extends FuelTankGun
+{
+
+    public FuelTankGun_TankSkyhawkX4_gn16()
+    {
+    }
+
+    public void matHighvis()
+    {
+        ((FuelTank_TankSkyhawkX4_gn16) bomb).matHighvis();
+    }
+
+    public void matSEAcamo()
+    {
+        ((FuelTank_TankSkyhawkX4_gn16) bomb).matSEAcamo();
+    }
+
+    static 
+    {
+        Class class1 = com.maddox.il2.objects.weapons.FuelTankGun_TankSkyhawkX4_gn16.class;
+        Property.set(class1, "bulletClass", (Object) com.maddox.il2.objects.weapons.FuelTank_TankSkyhawkX4_gn16.class);
+        Property.set(class1, "bullets", 1);
+        Property.set(class1, "shotFreq", 1.0F);
+        Property.set(class1, "external", 1);
+    }
+}
---
+++ Weapon_Generation2016/src/com/maddox/il2/objects/weapons/FuelTank_TankSkyhawkX4_gn16.java
@@ -0,0 +1,66 @@
+// 300 gal. Fueltank for S-3 Viking with 4x X style tailfin
+
+/*
+* Base color is low visibility dark gray for 1980s and later.
+
+* When you want high visibility white gray for 1960-1970s, add this code to mother Jets.
+
+    public void missionStarting()
+    {
+        super.missionStarting();
+        checkChangeWeaponColors();
+    }
+
+    private void checkChangeWeaponColors()
+    {
+        for(int i = 0; i < FM.CT.Weapons.length; i++)
+            if(FM.CT.Weapons[i] != null)
+            {
+                for(int j = 0; j < FM.CT.Weapons[i].length; j++)
+                {
+                    if(FM.CT.Weapons[i][j] instanceof FuelTankGun_TankSkyhawkT3_gn16)
+                        ((FuelTankGun_TankSkyhawkT3_gn16)FM.CT.Weapons[i][j]).matHighvis();
+                }
+            }
+    }
+
+*/
+
+
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+
+public class FuelTank_TankSkyhawkX4_gn16 extends FuelTank
+{
+
+    public FuelTank_TankSkyhawkX4_gn16()
+    {
+    }
+
+    public void matHighvis()
+    {
+        setMesh(Property.stringValue(getClass(), "mesh"));
+        mesh.materialReplace("Tank_Gloss", "Tank_GlossHV");
+        mesh.materialReplace("Tank_GlossP", "Tank_GlossHVP");
+        mesh.materialReplace("Tank_GlossQ", "Tank_GlossHVQ");
+    }
+
+    public void matSEAcamo()
+    {
+        setMesh(Property.stringValue(getClass(), "mesh"));
+        mesh.materialReplace("Tank_Gloss", "Tank_GlossSEA");
+        mesh.materialReplace("Tank_GlossP", "Tank_GlossSEAP");
+        mesh.materialReplace("Tank_GlossQ", "Tank_GlossSEAQ");
+    }
+
+    static 
+    {
+        Class class1 = com.maddox.il2.objects.weapons.FuelTank_TankSkyhawkX4_gn16.class;
+        Property.set(class1, "mesh", "3DO/Arms/TankSkyhawk_gn16/monox4.sim");
+        Property.set(class1, "kalibr", 0.6F);
+        Property.set(class1, "massa", 1150F);
+		Property.set(class1, "dragCoefficient", 0.32F); // Aerodynamic Drag Coefficient, Stock WWII droptanks=1.0F
+    }
+}
