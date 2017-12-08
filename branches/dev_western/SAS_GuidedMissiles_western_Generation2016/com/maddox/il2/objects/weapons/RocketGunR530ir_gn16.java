52527053f306fe10e4460ed160e6a03dab815ecc
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2017-12-08 05:51:35 +0000

    Update missiles in Weapon pack Generation2016 --- Add Matra R530 IRH and SARH, Matra R550 Magic1 and Magic2. Delete duplicated mat change codes with new Missile.class about AGM65 Marveriks. Debug AIM7 and AIM4 SARH values.

---
+++ branches/dev_western/SAS_GuidedMissiles_western_Generation2016/com/maddox/il2/objects/weapons/RocketGunR530ir_gn16.java
@@ -0,0 +1,17 @@
+// Source File Name: RocketGunR530ir_gn16.java
+// Author:           western0221
+// Last Modified by: western0221 2017-12-08
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class RocketGunR530ir_gn16 extends MissileGun implements RocketGunWithDelay {
+
+	static {
+		Class class1 = RocketGunR530ir_gn16.class;
+		Property.set(class1, "bulletClass", (Object) MissileR530ir_gn16.class);
+		Property.set(class1, "bullets", 1);
+		Property.set(class1, "shotFreq", 0.25F);
+		Property.set(class1, "sound", "weapon.rocketgun_132");
+	}
+}
