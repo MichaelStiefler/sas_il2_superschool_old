7994ef5f71bc2c5e18398fbc8c3caf18a178ef17
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2018-08-03 10:07:55 +0000

    Update Weapon pack Generation 2016-iii --- Add AGM-114A,C Hellfire and BGM-71A,C,D TOW

---
+++ branches/dev_western/SAS_GuidedMissiles_western_Generation2016/com/maddox/il2/objects/weapons/RocketGunBGM71C_gn16.java
@@ -0,0 +1,14 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class RocketGunBGM71C_gn16 extends MissileGun implements RocketGunWithDelay {
+
+	static {
+		Class class1 = RocketGunBGM71C_gn16.class;
+		Property.set(class1, "bulletClass", (Object) MissileBGM71C_gn16.class);
+		Property.set(class1, "bullets", 1);
+		Property.set(class1, "shotFreq", 4.0F);
+		Property.set(class1, "sound", "weapon.rocketgun_132");
+	}
+}
