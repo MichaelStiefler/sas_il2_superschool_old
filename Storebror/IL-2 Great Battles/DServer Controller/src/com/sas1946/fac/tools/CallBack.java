a833497fcc4ac2a25c268afa47def1ab61c5e7ea
Michael Stiefler <michael.stiefler@gmail.com>
2018-08-03 12:44:34 +0000

    Initial WIP commit... just basic functionality for testing so far.

---
+++ Storebror/IL-2 Great Battles/DServer Controller/src/com/sas1946/fac/tools/CallBack.java
@@ -0,0 +1,5 @@
+package com.sas1946.fac.tools;
+
+public abstract class CallBack<TRet, TArg1, TArg2> {
+    public abstract TRet call(TArg1 val1, TArg2 val2);
+}
---
+++ Storebror/IL-2 Great Battles/DServer Controller/src/com/sas1946/fac/tools/DServerController.java
@@ -0,0 +1,163 @@
+package com.sas1946.fac.tools;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+public class DServerController {
+
+    private final static int        MAX_LOG_LINES = 20;
+    protected Shell                 shlDserverController;
+    private final FormToolkit       formToolkit   = new FormToolkit(Display.getDefault());
+    private List                    logList;
+    private Label                   lblTickDelayValue;
+    private final boolean           authenticated = false;
+
+    CallBack<Void, Integer, String> rConCallback  = new CallBack<Void, Integer, String>() {
+                                                      @Override
+                                                      public Void call(Integer val1, String val2) {
+                                                          DServerController.this.rConClientCallback(val1, val2);
+                                                          return null;
+                                                      }
+                                                  };
+
+    private void tickDelayWatcherCallback() {
+        Display.getDefault().asyncExec(() -> DServerController.this.lblTickDelayValue.setText(Float.toString(TickDelayWatcher.getInstance().getTickDelay())));
+    }
+
+    private void rConClientCallback(Integer returnCode, String feedback) {
+        Display.getDefault().asyncExec(() -> {
+            DServerController.this.addLog(feedback);
+            DServerController.this.handleRConClientCallback(returnCode, feedback);
+        });
+    }
+
+    private void addLog(String logLine) {
+        Display.getDefault().asyncExec(() -> {
+            while (DServerController.this.logList.getItemCount() > MAX_LOG_LINES) {
+                DServerController.this.logList.remove(0);
+            }
+            DServerController.this.logList.add(logLine);
+        });
+    }
+
+    private void handleRConClientCallback(int returnCode, String feedback) {
+        switch (returnCode) {
+
+            case RConClient.RCONCLIENT_MESSAGE_RECEIVED:
+                this.handleRConClientMessageReceived(RConClient.getInstance().getLastCommand(), feedback);
+                break;
+
+            case RConClient.RCONCLIENT_CONNECTED:
+                RConClient.getInstance().auth(Settings.getInstance().getrConSettings().getUser(), Settings.getInstance().getrConSettings().getPass());
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    private void handleRConClientMessageReceived(RConClient.COMMAND command, String feedback) {
+        switch (command) {
+            case none:
+                break;
+            case auth:
+                RConClient.getInstance().spsget();
+                break;
+            case spsget:
+                //RConClient.getInstance().spsget();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Launch the application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            final DServerController window = new DServerController();
+            window.open();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Open the window.
+     */
+    public void open() {
+        final Display display = Display.getDefault();
+        this.createContents();
+        this.shlDserverController.open();
+        this.shlDserverController.layout();
+
+        TickDelayWatcher.getInstance().setTickDelayUpdateCallback(() -> this.tickDelayWatcherCallback());
+        RConClient.getInstance().setRConCallback(this.rConCallback);
+        RConClient.getInstance().connect(Settings.getInstance().getrConSettings().getAddress(), Settings.getInstance().getrConSettings().getPort());
+
+        while (!this.shlDserverController.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+    }
+
+    /**
+     * Create contents of the window.
+     */
+    protected void createContents() {
+        this.shlDserverController = new Shell();
+        this.shlDserverController.setSize(800, 373);
+        this.shlDserverController.setText("DServer Controller");
+
+        this.logList = new List(this.shlDserverController, SWT.BORDER | SWT.V_SCROLL);
+        this.logList.setBounds(10, 10, 764, 241);
+        this.formToolkit.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_BACKGROUND));
+        this.formToolkit.adapt(this.logList, true, true);
+
+        final Button btnCheckButton = new Button(this.shlDserverController, SWT.CHECK);
+        btnCheckButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                final Button btn = (Button) e.getSource();
+                btn.setSelection(DServerController.this.authenticated);
+            }
+        });
+        btnCheckButton.setBounds(10, 257, 96, 16);
+        this.formToolkit.adapt(btnCheckButton, true, true);
+        btnCheckButton.setText("Authenticated");
+
+        final Label lblSps = new Label(this.shlDserverController, SWT.NONE);
+        lblSps.setBounds(10, 279, 55, 15);
+        this.formToolkit.adapt(lblSps, true, true);
+        lblSps.setText("SPS:");
+
+        final Label lblNewLabel = new Label(this.shlDserverController, SWT.NONE);
+        lblNewLabel.setBounds(71, 279, 55, 15);
+        this.formToolkit.adapt(lblNewLabel, true, true);
+        lblNewLabel.setText("0.0");
+
+        Label lblTickDelay = new Label(shlDserverController, SWT.NONE);
+        lblTickDelay.setText("Tick Delay:");
+        lblTickDelay.setBounds(10, 300, 55, 15);
+        formToolkit.adapt(lblTickDelay, true, true);
+
+        lblTickDelayValue = new Label(shlDserverController, SWT.NONE);
+        lblTickDelayValue.setText("0.0");
+        lblTickDelayValue.setBounds(71, 300, 55, 15);
+        formToolkit.adapt(lblTickDelayValue, true, true);
+
+    }
+}
