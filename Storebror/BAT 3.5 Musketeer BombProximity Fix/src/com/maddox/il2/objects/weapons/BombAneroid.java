ad7922a380c45e72bbe2e58707f172881cf38188
Michael Stiefler <michael.stiefler@gmail.com>
2018-11-17 16:31:22 +0000

    v2 of the Bombs fix for BAT 3.5

--- Storebror/BAT 3.5 Musketeer BombProximity Fix/src/com/maddox/il2/objects/weapons/BombAneroid.java
+++ Storebror/BAT 3.5 Musketeer BombProximity Fix/src/com/maddox/il2/objects/weapons/BombAneroid.java
@@ -1,9 +1,8 @@
 package com.maddox.il2.objects.weapons;
 
-import com.maddox.il2.ai.World;
 import com.maddox.rts.Time;
 
-public class BombProximity extends Bomb {
+public class BombAneroid extends Bomb {
 
     public int getFuzeType() {
         return this.fuze == null ? -1 : this.fuze.getFuzeType();
@@ -11,9 +10,9 @@ public class BombProximity extends Bomb {
 
     public void interpolateTick() {
         super.interpolateTick();
-        if ((this.getFuzeType() == 10) && this.fuze.isArmed()) {
+        if ((this.getFuzeType() == 8) && this.fuze.isArmed()) {
             this.pos.getTime(Time.current(), Bomb.P);
-            if (this.pos.getRelPoint().z < (this.fuze.getDetonationDelay() + World.land().HQ(this.pos.getRelPoint().x, this.pos.getRelPoint().y))) {
+            if (this.pos.getRelPoint().z < this.fuze.getDetonationDelay()) {
                 this.doMidAirExplosion();
             }
         }
