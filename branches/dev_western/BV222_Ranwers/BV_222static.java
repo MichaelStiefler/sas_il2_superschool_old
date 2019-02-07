d06a7fd4ca93d83beea3c09956ac4ff842868a29
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-02-07 03:30:50 +0000

    update BV-222 (Ranwers) closed Beta version --- many cockpit gauges added working, FM fix about land and float on the sea

---
+++ branches/dev_western/BV222_Ranwers/BV_222static.java
@@ -0,0 +1,24 @@
+
+package com.maddox.il2.objects.vehicles.planes;
+
+
+public abstract class BV_222static
+{
+    public static class BV_222_1 extends PlaneGeneric
+    {
+
+        public BV_222_1()
+        {
+        }
+    }
+
+    public BV_222static()
+    {
+    }
+
+
+    static 
+    {
+        new PlaneGeneric.SPAWN(com.maddox.il2.objects.vehicles.planes.BV_222static.BV_222_1.class);
+    }
+}
