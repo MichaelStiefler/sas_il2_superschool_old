62c0bbe98403d9b134593e3eb54c84ce1ae3b31d
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-02-25 17:47:46 +0000

    BV 222 by Ranwers developing --- more reworks on gunner cockpits

--- branches/dev_western/BV222_Ranwers/CockpitBV_222_NGunner.java
+++ branches/dev_western/BV222_Ranwers/CockpitBV_222_NGunner.java
@@ -2,14 +2,11 @@
 package com.maddox.il2.objects.air;
 
 import com.maddox.il2.ai.BulletEmitter;
-import com.maddox.il2.engine.HierMesh;
-import com.maddox.il2.engine.Orient;
+import com.maddox.il2.engine.*;
 import com.maddox.il2.fm.*;
 import com.maddox.rts.CLASS;
 import com.maddox.rts.Property;
 
-// Referenced classes of package com.maddox.il2.objects.air:
-//            CockpitGunner
 
 public class CockpitBV_222_NGunner extends CockpitGunner
 {
@@ -17,80 +14,113 @@ public class CockpitBV_222_NGunner extends CockpitGunner
     public void moveGun(Orient orient)
     {
         super.moveGun(orient);
-        float f = -orient.getYaw();
-        float f1 = orient.getTangage();
-        mesh.chunkSetAngles("Turret1A", 0.0F, -f, 0.0F);
-        mesh.chunkSetAngles("Turret1B", 0.0F, f1, 0.0F);
+        super.mesh.chunkSetAngles("Body", 0.0F, 0.0F, 0.0F);
+        super.mesh.chunkSetAngles("TurretRA", 0.0F, -orient.getYaw(), 0.0F);
+        super.mesh.chunkSetAngles("TurretRB", 0.0F, orient.getTangage(), 0.0F);
     }
 
     public void clipAnglesGun(Orient orient)
     {
-        if(!isRealMode())
-            return;
-        if(!aiTurret().bIsOperable)
-        {
-            orient.setYPR(0.0F, 0.0F, 0.0F);
-            return;
-        }
-        float f = orient.getYaw();
-        float f1 = orient.getTangage();
-        if(f < -20F)
-            f = -20F;
-        if(f > 20F)
-            f = 20F;
-        if(f1 > 20F)
-            f1 = 20F;
-        if(f1 < -20F)
-            f1 = -20F;
-        orient.setYPR(f, f1, 0.0F);
-        orient.wrap();
+        if(isRealMode())
+            if(!aiTurret().bIsOperable)
+            {
+                orient.setYPR(0.0F, 0.0F, 0.0F);
+            } else
+            {
+                float f = orient.getYaw();
+                float f1 = orient.getTangage();
+                if(f < -20F)
+                    f = -20F;
+                if(f > 20F)
+                    f = 20F;
+                if(f1 > 20F)
+                    f1 = 20F;
+                if(f1 < -20F)
+                    f1 = -20F;
+                orient.setYPR(f, f1, 0.0F);
+                orient.wrap();
+            }
     }
 
     protected void interpTick()
     {
-        if(!isRealMode())
-            return;
-        if(emitter == null || !emitter.haveBullets() || !aiTurret().bIsOperable)
-            bGunFire = false;
-        fm.CT.WeaponControl[weaponControlNum()] = bGunFire;
-        if(bGunFire)
+        if(isRealMode())
         {
-            if(iCocking > 0)
+            if(super.emitter == null || !super.emitter.haveBullets() || !aiTurret().bIsOperable)
+                super.bGunFire = false;
+            (super.fm).CT.WeaponControl[weaponControlNum()] = super.bGunFire;
+            if(super.bGunFire)
+            {
+                if(hook1 == null)
+                    hook1 = new HookNamed(aircraft(), "_MGUN08");
+                doHitMasterAircraft(aircraft(), hook1, "_MGUN08");
+                if(iCocking > 0)
+                    iCocking = 0;
+                else
+                    iCocking = 1;
+                iNewVisDrums = (int)((float)super.emitter.countBullets() / 250F);
+                if(iNewVisDrums < iOldVisDrums)
+                {
+                    iOldVisDrums = iNewVisDrums;
+                    super.mesh.chunkVisible("DrumR1", iNewVisDrums > 1);
+                    super.mesh.chunkVisible("DrumR2", iNewVisDrums > 0);
+                    sfxClick(13);
+                }
+            } else
+            {
                 iCocking = 0;
-            else
-                iCocking = 1;
-        } else
-        {
-            iCocking = 0;
+            }
+            resetYPRmodifier();
+            Cockpit.xyz[0] = -0.07F * (float)iCocking;
+            super.mesh.chunkSetLocate("LeverR", Cockpit.xyz, Cockpit.ypr);
         }
-        resetYPRmodifier();
-        xyz[1] = -0.07F * (float)iCocking;
-        ypr[1] = 0.0F;
-        mesh.chunkSetLocate("Turret1C", xyz, ypr);
     }
 
     public void doGunFire(boolean flag)
     {
-        if(!isRealMode())
-            return;
-        if(emitter == null || !emitter.haveBullets() || !aiTurret().bIsOperable)
-            bGunFire = false;
-        else
-            bGunFire = flag;
-        fm.CT.WeaponControl[weaponControlNum()] = bGunFire;
+        if(isRealMode())
+        {
+            if(super.emitter == null || !super.emitter.haveBullets() || !aiTurret().bIsOperable)
+                super.bGunFire = false;
+            else
+                super.bGunFire = flag;
+            (super.fm).CT.WeaponControl[weaponControlNum()] = super.bGunFire;
+        }
     }
 
     public CockpitBV_222_NGunner()
     {
-        super("3DO/Cockpit/G4M1-11-NGun/NGunnerBV222.him", "he111");
+        super("3DO/Cockpit/He-111H-2-RGun/NGunnerBV222.him", "he111_gunner");
+        hook1 = null;
         iCocking = 0;
+        iOldVisDrums = 2;
+        iNewVisDrums = 2;
+    }
+
+    public void toggleLight()
+    {
+        super.cockpitLightControl = !super.cockpitLightControl;
+        if(super.cockpitLightControl)
+        {
+            super.mesh.chunkVisible("Flare", true);
+            setNightMats(true);
+        } else
+        {
+            super.mesh.chunkVisible("Flare", false);
+            setNightMats(false);
+        }
     }
 
     public void reflectCockpitState()
     {
+        if((super.fm).AS.astateCockpitState != 0)
+            super.mesh.chunkVisible("Holes_D1", true);
     }
 
+    private Hook hook1;
     private int iCocking;
+    private int iOldVisDrums;
+    private int iNewVisDrums;
 
     static 
     {
