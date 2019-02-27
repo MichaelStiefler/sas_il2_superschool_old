c7e566aab024a8717153b2ffaa528c967d18a493
Michael Stiefler <michael.stiefler@gmail.com>
2019-02-27 16:02:28 +0000

    BAT 3.5 JTW BombGunTorpMk34 Fix

---
+++ Storebror/BAT 3.5 JTW BombGunTorpMk34 Fix/src/com/maddox/il2/objects/weapons/BombGunTorpMk34.java
@@ -0,0 +1,22 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.rts.Property;
+
+public class BombGunTorpMk34 extends TorpedoGun {
+
+    public void setBombDelay(float f) {
+        this.bombDelay = 0.0F;
+        if (this.bomb != null) {
+            this.bomb.delayExplosion = this.bombDelay;
+        }
+    }
+
+    static {
+        Class class1 = BombGunTorpMk34.class;
+        Property.set(class1, "bulletClass", (Object) BombTorpMk34.class);
+        Property.set(class1, "bullets", 1);
+        Property.set(class1, "shotFreq", 0.1F);
+        Property.set(class1, "external", 1);
+        Property.set(class1, "sound", "weapon.bombgun_torpedo");
+    }
+}
