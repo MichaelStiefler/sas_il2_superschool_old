0e4a83b4160c64026372e7484190b97619b7a627
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-02-27 16:02:28 +0000

    BAT 3.5 JTW BombGunTorpMk34 Fix

---
+++ trunk/Storebror/BAT 3.5 JTW BombGunTorpMk34 Fix/src/com/maddox/il2/objects/weapons/BombGunTorpMk34_B24rear.java
@@ -0,0 +1,19 @@
+package com.maddox.il2.objects.weapons;
+
+import com.maddox.JGP.Point3d;
+
+public class BombGunTorpMk34_B24rear extends BombGunTorpMk34 {
+
+    public void loadBullets(int i) {
+        super.loadBullets(i);
+        if (this.bullets() == 0) {
+            return;
+        } else {
+            Point3d point3d = this.bomb.pos.getRelPoint();
+            point3d.x -= 0.23D;
+            point3d.y -= 0.19D;
+            this.bomb.pos.setRel(point3d);
+            return;
+        }
+    }
+}
