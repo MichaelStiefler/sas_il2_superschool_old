6f3bb1ec54c7b9fdaa896a3993cc494891b55757
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-03-06 14:47:51 +0000

    BV 222 for BAT 3.6 WAW

---
+++ trunk/Storebror/BAT 3.6 BV 222/src/com/maddox/il2/objects/vehicles/planes/BV_222static.java
@@ -0,0 +1,17 @@
+
+package com.maddox.il2.objects.vehicles.planes;
+
+public abstract class BV_222static {
+    public static class BV_222_1 extends PlaneGeneric {
+
+        public BV_222_1() {
+        }
+    }
+
+    public BV_222static() {
+    }
+
+    static {
+        new PlaneGeneric.SPAWN(BV_222static.BV_222_1.class);
+    }
+}
