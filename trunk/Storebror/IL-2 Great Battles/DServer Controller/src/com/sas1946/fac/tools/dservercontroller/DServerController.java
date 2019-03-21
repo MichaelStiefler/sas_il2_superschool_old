6358c0c4adec23c14ff3a824eed3559d1ac73289
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-03-21 15:15:03 +0000

    DServer Controller Update. Bear with me: I've got no idea what has changed since last commit. Just thought I'd commit what I have to get things started again.

--- trunk/Storebror/IL-2 Great Battles/DServer Controller/src/com/sas1946/fac/tools/dservercontroller/DServerController.java
+++ trunk/Storebror/IL-2 Great Battles/DServer Controller/src/com/sas1946/fac/tools/dservercontroller/DServerController.java
@@ -139,7 +139,7 @@ public class DServerController {
                 RConClient.getInstance().stopTimer();
               }
             });
-        
+
         MySqlConnector mySqlConnector = MySqlConnector.getInstance();
 
         TickDelayWatcher.getInstance().setTickDelayUpdateCallback(() -> this.tickDelayWatcherCallback());
