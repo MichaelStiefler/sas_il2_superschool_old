c48dac0a26ab8a291d7e598e480142e024bff03c
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-09-19 15:18:30 +0000

    Ultrapack 3.3 Bigfoot Patch 1 release

---
+++ trunk/Storebror/UP3.3 Patch 1/src/com/maddox/il2/objects/air/BattleMkII.java
@@ -0,0 +1,166 @@
+package com.maddox.il2.objects.air;
+
+import java.io.IOException;
+
+import com.maddox.il2.game.AircraftHotKeys;
+import com.maddox.il2.game.HUD;
+import com.maddox.rts.NetMsgGuaranted;
+import com.maddox.rts.NetMsgInput;
+import com.maddox.rts.Property;
+
+public class BattleMkII extends FaireyBattle implements TypeBomber {
+
+    public BattleMkII() {
+        this.fSightCurAltitude = 300F;
+        this.fSightCurSpeed = 50F;
+        this.fSightCurForwardAngle = 0.0F;
+        this.fSightSetForwardAngle = 0.0F;
+        this.fSightCurSideslip = 0.0F;
+    }
+    
+    public void onAircraftLoaded() {
+        super.onAircraftLoaded();
+        if (!this.thisWeaponsName.startsWith("4x") && !this.thisWeaponsName.startsWith("6x")) this.FM.CT.bHasBayDoorControl = false;
+    }
+
+    public boolean turretAngles(int i, float af[]) {
+        boolean flag = super.turretAngles(i, af);
+        float f = -af[0];
+        float f1 = af[1];
+        switch (i) {
+            case 0:
+                if (f < -31F) {
+                    f = -31F;
+                    flag = false;
+                }
+                if (f > 31F) {
+                    f = 31F;
+                    flag = false;
+                }
+                if (f1 < -10F) {
+                    f1 = -10F;
+                    flag = false;
+                }
+                if (f1 > 52F) {
+                    f1 = 52F;
+                    flag = false;
+                }
+                break;
+        }
+        af[0] = -f;
+        af[1] = f1;
+        return flag;
+    }
+
+    public boolean typeBomberToggleAutomation() {
+        return false;
+    }
+
+    public void typeBomberAdjDistanceReset() {
+        this.fSightCurForwardAngle = 0.0F;
+    }
+
+    public void typeBomberAdjDistancePlus() {
+        this.fSightCurForwardAngle += 0.2F;
+        if (this.fSightCurForwardAngle > 75F) this.fSightCurForwardAngle = 75F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightElevation", new Object[] { new Integer((int) (this.fSightCurForwardAngle * 1.0F)) });
+    }
+
+    public void typeBomberAdjDistanceMinus() {
+        this.fSightCurForwardAngle -= 0.2F;
+        if (this.fSightCurForwardAngle < -15F) this.fSightCurForwardAngle = -15F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightElevation", new Object[] { new Integer((int) (this.fSightCurForwardAngle * 1.0F)) });
+    }
+
+    public void typeBomberAdjSideslipReset() {
+        this.fSightCurSideslip = 0.0F;
+    }
+
+    public void typeBomberAdjSideslipPlus() {
+        this.fSightCurSideslip++;
+        if (this.fSightCurSideslip > 45F) this.fSightCurSideslip = 45F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightSlip", new Object[] { new Integer((int) (this.fSightCurSideslip * 1.0F)) });
+    }
+
+    public void typeBomberAdjSideslipMinus() {
+        this.fSightCurSideslip--;
+        if (this.fSightCurSideslip < -45F) this.fSightCurSideslip = -45F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightSlip", new Object[] { new Integer((int) (this.fSightCurSideslip * 1.0F)) });
+    }
+
+    public void typeBomberAdjAltitudeReset() {
+        this.fSightCurAltitude = 300F;
+    }
+
+    public void typeBomberAdjAltitudePlus() {
+        this.fSightCurAltitude += 10F;
+        if (this.fSightCurAltitude > 6000F) this.fSightCurAltitude = 6000F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightAltitude", new Object[] { new Integer((int) this.fSightCurAltitude) });
+    }
+
+    public void typeBomberAdjAltitudeMinus() {
+        this.fSightCurAltitude -= 10F;
+        if (this.fSightCurAltitude < 300F) this.fSightCurAltitude = 300F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightAltitude", new Object[] { new Integer((int) this.fSightCurAltitude) });
+    }
+
+    public void typeBomberAdjSpeedReset() {
+        this.fSightCurSpeed = 50F;
+    }
+
+    public void typeBomberAdjSpeedPlus() {
+        this.fSightCurSpeed += 5F;
+        if (this.fSightCurSpeed > 650F) this.fSightCurSpeed = 650F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightSpeed", new Object[] { new Integer((int) this.fSightCurSpeed) });
+    }
+
+    public void typeBomberAdjSpeedMinus() {
+        this.fSightCurSpeed -= 5F;
+        if (this.fSightCurSpeed < 50F) this.fSightCurSpeed = 50F;
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "BombsightSpeed", new Object[] { new Integer((int) this.fSightCurSpeed) });
+    }
+
+    public void typeBomberUpdate(float f) {
+        double d = this.fSightCurSpeed / 3.6D * Math.sqrt(this.fSightCurAltitude * 0.203873598D);
+        d -= this.fSightCurAltitude * this.fSightCurAltitude * 0.00001419D;
+        this.fSightSetForwardAngle = (float) Math.toDegrees(Math.atan(d / this.fSightCurAltitude));
+    }
+
+    public void typeBomberReplicateToNet(NetMsgGuaranted netmsgguaranted) throws IOException {
+        netmsgguaranted.writeFloat(this.fSightCurAltitude);
+        netmsgguaranted.writeFloat(this.fSightCurSpeed);
+        netmsgguaranted.writeFloat(this.fSightCurForwardAngle);
+        netmsgguaranted.writeFloat(this.fSightCurSideslip);
+    }
+
+    public void typeBomberReplicateFromNet(NetMsgInput netmsginput) throws IOException {
+        this.fSightCurAltitude = netmsginput.readFloat();
+        this.fSightCurSpeed = netmsginput.readFloat();
+        this.fSightCurForwardAngle = netmsginput.readFloat();
+        this.fSightCurSideslip = netmsginput.readFloat();
+    }
+
+    public float fSightCurAltitude;
+    public float fSightCurSpeed;
+    public float fSightCurForwardAngle;
+    public float fSightSetForwardAngle;
+    public float fSightCurSideslip;
+
+    static {
+        Class class1 = BattleMkII.class;
+        new NetAircraft.SPAWN(class1);
+        Property.set(class1, "iconFar_shortClassName", "FaireyBattle");
+        Property.set(class1, "meshName", "3DO/Plane/FaireyBattle(Multi1)/hier.him");
+        Property.set(class1, "PaintScheme", new PaintSchemeBMPar00());
+        Property.set(class1, "meshName_gb", "3DO/Plane/FaireyBattle(gb)/hier.him");
+        Property.set(class1, "PaintScheme_gb", new PaintSchemeBMPar00());
+        Property.set(class1, "yearService", 1937F);
+        Property.set(class1, "yearExpired", 1941.5F);
+        Property.set(class1, "FlightModel", "FlightModels/BattleMkII.fmd");
+        Property.set(class1, "cockpitClass", new Class[] { CockpitBattleMkII.class, Cockpit_BombsightOPB.class, CockpitBattleMkII_TGunner.class });
+        Property.set(class1, "LOSElevation", 0.7394F);
+        Aircraft.weaponTriggersRegister(class1, new int[] { 0, 10, 9, 3, 3, 3, 3, 3, 3, 3 });
+        Aircraft.weaponHooksRegister(class1, new String[] { "_MGUN01", "_MGUN02", "_ExternalDev01", "_ExternalBomb01", "_BombSpawn01",
+                "_BombSpawn02", "_BombSpawn03", "_BombSpawn04", "_BombSpawn05", "_BombSpawn06" });
+    }
+}
