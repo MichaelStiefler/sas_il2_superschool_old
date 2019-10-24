437c31a126d41c5a179d6be9ecae4dd746ddab0b
Michael Stiefler <michael.stiefler@gmail.com>
2019-10-24 13:40:41 +0000

    Updated Common Utils 1.13

--- Storebror/SAS Common Utils DLL/CommonUtils/XpTools.cpp
+++ Storebror/SAS Common Utils DLL/CommonUtils/XpTools.cpp
@@ -1,14 +1,18 @@
 #include "stdafx.h"
 //#include <winternl.h>
 //#include <limits.h>
-#include <assert.h>
+//#include <assert.h> // TODO: Removed by SAS~Storebror. Don't _ever_ crash with assertion errors, keep going!
 #include <tchar.h>
 //#include <WinIoCtl.h>
 
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning( disable : 4996 )
 
-#pragma comment(lib, "lib/ntdll.lib")
+// TODO: Changed SAS~Storebror: Replace static link to ntdll.dll by dynamically linking the file on demand.
+//       Reason: There's many PCs out there which have this file removed/damaged, and we really only ever
+//               need it on ancient XP machines, so there's no need to bother Vista/7/10 users with a missing
+//               file dependency on ntdll.dll.
+//#pragma comment(lib, "lib/ntdll.lib")
 
 #define METHOD_BUFFERED                 0
 #define FILE_ANY_ACCESS                 0
@@ -26,13 +30,25 @@ struct UNICODE_STRING { USHORT Length; USHORT MaximumLength; PWSTR Buffer; };
 struct MOUNTMGR_TARGET_NAME { USHORT DeviceNameLength; WCHAR DeviceName[1]; };
 struct MOUNTMGR_VOLUME_PATHS { ULONG MultiSzLength; WCHAR MultiSz[1]; };
 
-extern "C" NTSYSAPI NTSTATUS NTAPI NtQueryObject(IN HANDLE Handle OPTIONAL,
+//extern "C" NTSYSAPI NTSTATUS NTAPI NtQueryObject(IN HANDLE Handle OPTIONAL,
+//	IN OBJECT_INFORMATION_CLASS ObjectInformationClass,
+//	OUT PVOID ObjectInformation OPTIONAL, IN ULONG ObjectInformationLength,
+//	OUT PULONG ReturnLength OPTIONAL);
+//extern "C" NTSYSAPI NTSTATUS NTAPI NtQueryInformationFile(IN HANDLE FileHandle,
+//	OUT PIO_STATUS_BLOCK IoStatusBlock, OUT PVOID FileInformation,
+//	IN ULONG Length, IN FILE_INFORMATION_CLASS FileInformationClass);
+
+typedef NTSTATUS(NTAPI* NTQUERYOBJECT)(IN HANDLE Handle OPTIONAL,
 	IN OBJECT_INFORMATION_CLASS ObjectInformationClass,
 	OUT PVOID ObjectInformation OPTIONAL, IN ULONG ObjectInformationLength,
 	OUT PULONG ReturnLength OPTIONAL);
-extern "C" NTSYSAPI NTSTATUS NTAPI NtQueryInformationFile(IN HANDLE FileHandle,
+
+typedef NTSTATUS(NTAPI* NTQUERYINFORMATIONFILE)(IN HANDLE FileHandle,
 	OUT PIO_STATUS_BLOCK IoStatusBlock, OUT PVOID FileInformation,
 	IN ULONG Length, IN FILE_INFORMATION_CLASS FileInformationClass);
+
+NTQUERYOBJECT NtQueryObject = NULL;
+NTQUERYINFORMATIONFILE NtQueryInformationFile = NULL;
 
 #define MOUNTMGRCONTROLTYPE ((ULONG) 'm')
 #define IOCTL_MOUNTMGR_QUERY_DOS_VOLUME_PATH \
@@ -50,14 +66,44 @@ LPWSTR GetXpFilePath(HANDLE hFile)
 {
 	static ANY_BUFFER nameFull, nameRel, nameMnt;
 	ULONG returnedLength; IO_STATUS_BLOCK iosb; NTSTATUS status;
+
+	BOOL bNeedsFreeLibrary = FALSE;
+
+	if (NtQueryObject == NULL || NtQueryInformationFile == NULL) {
+		HMODULE ntdll = GetModuleHandle(_T("ntdll.dll"));
+		if (ntdll == NULL || ntdll == INVALID_HANDLE_VALUE) {
+			bNeedsFreeLibrary = TRUE;
+			HMODULE ntdll = LoadLibrary(_T("ntdll.dll"));
+			if (ntdll == NULL || ntdll == INVALID_HANDLE_VALUE) {
+				return NULL;
+			}
+		}
+		if (NtQueryObject == NULL) {
+			NtQueryObject = (NTQUERYOBJECT)GetProcAddress(ntdll, "NtQueryObject");
+			if (NtQueryObject == NULL || NtQueryObject == INVALID_HANDLE_VALUE) {
+				return NULL;
+			}
+		}
+		if (NtQueryInformationFile == NULL) {
+			NtQueryInformationFile = (NTQUERYINFORMATIONFILE)GetProcAddress(ntdll, "NtQueryInformationFile");
+			if (NtQueryInformationFile == NULL || NtQueryInformationFile == INVALID_HANDLE_VALUE) {
+				return NULL;
+			}
+		}
+		if (bNeedsFreeLibrary) FreeLibrary(ntdll);
+	}
+
 	status = NtQueryObject(hFile, ObjectNameInformation,
 		nameFull.Buffer, sizeof(nameFull.Buffer), &returnedLength);
-	assert(status == 0);
+	if (status != 0) return NULL;
+	//assert(status == 0);
 	status = NtQueryInformationFile(hFile, &iosb, nameRel.Buffer,
 		sizeof(nameRel.Buffer), FileNameInformation);
-	assert(status == 0);
+	if (status != 0) return NULL;
+	//assert(status == 0);
 	//I'm not sure how this works with network paths...
-	assert(nameFull.UnicodeString.Length >= nameRel.NameInfo.FileNameLength);
+	if (nameFull.UnicodeString.Length < nameRel.NameInfo.FileNameLength) return NULL;
+	//assert(nameFull.UnicodeString.Length >= nameRel.NameInfo.FileNameLength);
 	nameMnt.TargetName.DeviceNameLength = (USHORT)(
 		nameFull.UnicodeString.Length - nameRel.NameInfo.FileNameLength);
 	wcsncpy(nameMnt.TargetName.DeviceName, nameFull.UnicodeString.Buffer,
@@ -72,7 +118,8 @@ LPWSTR GetXpFilePath(HANDLE hFile)
 			IOCTL_MOUNTMGR_QUERY_DOS_VOLUME_PATH, &nameMnt,
 			sizeof(nameMnt), &nameMnt, sizeof(nameMnt),
 			&bytesReturned, NULL);
-		assert(success && nameMnt.TargetPaths.MultiSzLength > 0);
+		if (!success || nameMnt.TargetPaths.MultiSzLength <= 0) return NULL;
+		//assert(success && nameMnt.TargetPaths.MultiSzLength > 0);
 		wcsncat(nameMnt.TargetPaths.MultiSz, nameRel.NameInfo.FileName,
 			nameRel.NameInfo.FileNameLength / sizeof(WCHAR));
 		return nameMnt.TargetPaths.MultiSz;
