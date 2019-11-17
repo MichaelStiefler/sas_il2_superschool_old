8c7b956ed261b556c5788b7a3d0c5e0ea1016528
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2019-11-17 18:14:29 +0000

    Nakajima G10N1 Fugaku 4.09m Rebel Addons v1.00 Release

---
+++ trunk/Storebror/G10N1 Fugaku 4.09m Rebel Addons/src/com/maddox/il2/objects/air/TypeGuidedBombCarrier.java
@@ -0,0 +1,12 @@
+package com.maddox.il2.objects.air;
+
+public interface TypeGuidedBombCarrier {
+
+	public abstract boolean typeGuidedBombCgetIsGuiding();
+
+	public abstract void typeGuidedBombCsetIsGuiding(boolean flag);
+
+	public abstract boolean typeGuidedBombCisMasterAlive();
+
+	public abstract void typeGuidedBombCsetMasterAlive(boolean flag);
+}
