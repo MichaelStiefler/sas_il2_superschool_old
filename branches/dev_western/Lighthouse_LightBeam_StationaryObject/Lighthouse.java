56d78ef28c3d8ac53527eae969fd563941553096
western0221 <western0221@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2020-01-05 20:20:13 +0000

    Lighthouse Light Beam (ver 11/Jan./2015)

---
+++ branches/dev_western/Lighthouse_LightBeam_StationaryObject/Lighthouse.java
@@ -0,0 +1,121 @@
+
+package com.maddox.il2.objects.vehicles.stationary;
+
+
+public abstract class Lighthouse
+{
+    public static class LighthouseKit1_dual_10_Port_Lighthouse extends LighthouseGeneric
+    {
+
+        public LighthouseKit1_dual_10_Port_Lighthouse()
+        {
+        }
+    }
+
+    public static class LighthouseKit2_dual_20_Port_Lighthouse extends LighthouseGeneric
+    {
+
+        public LighthouseKit2_dual_20_Port_Lighthouse()
+        {
+        }
+    }
+
+    public static class LighthouseKit3_dual_30_Port_Lighthouse extends LighthouseGeneric
+    {
+
+        public LighthouseKit3_dual_30_Port_Lighthouse()
+        {
+        }
+    }
+
+    public static class LighthouseKit4_single_10_Port_Lighthouse extends LighthouseGeneric
+    {
+
+        public LighthouseKit4_single_10_Port_Lighthouse()
+        {
+        }
+    }
+
+    public static class LighthouseKit5_single_20_Port_Lighthouse extends LighthouseGeneric
+    {
+
+        public LighthouseKit5_single_20_Port_Lighthouse()
+        {
+        }
+    }
+
+    public static class LighthouseKit6_single_30_Port_Lighthouse extends LighthouseGeneric
+    {
+
+        public LighthouseKit6_single_30_Port_Lighthouse()
+        {
+        }
+    }
+
+
+    public static class Lighthouse1_dual_10 extends LighthouseGeneric
+    {
+
+        public Lighthouse1_dual_10()
+        {
+        }
+    }
+
+    public static class Lighthouse2_dual_20 extends LighthouseGeneric
+    {
+
+        public Lighthouse2_dual_20()
+        {
+        }
+    }
+
+    public static class Lighthouse3_dual_30 extends LighthouseGeneric
+    {
+
+        public Lighthouse3_dual_30()
+        {
+        }
+    }
+
+    public static class Lighthouse4_single_10 extends LighthouseGeneric
+    {
+
+        public Lighthouse4_single_10()
+        {
+        }
+    }
+
+    public static class Lighthouse5_single_20 extends LighthouseGeneric
+    {
+
+        public Lighthouse5_single_20()
+        {
+        }
+    }
+
+    public static class Lighthouse6_single_30 extends LighthouseGeneric
+    {
+
+        public Lighthouse6_single_30()
+        {
+        }
+    }
+
+
+
+    static 
+    {
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.LighthouseKit1_dual_10_Port_Lighthouse.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.LighthouseKit2_dual_20_Port_Lighthouse.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.LighthouseKit3_dual_30_Port_Lighthouse.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.LighthouseKit4_single_10_Port_Lighthouse.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.LighthouseKit5_single_20_Port_Lighthouse.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.LighthouseKit6_single_30_Port_Lighthouse.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.Lighthouse1_dual_10.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.Lighthouse2_dual_20.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.Lighthouse3_dual_30.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.Lighthouse4_single_10.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.Lighthouse5_single_20.class);
+        new LighthouseGeneric.SPAWN(com.maddox.il2.objects.vehicles.stationary.Lighthouse.Lighthouse6_single_30.class);
+    }
+}
