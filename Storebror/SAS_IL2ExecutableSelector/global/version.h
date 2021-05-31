b0cc4b356424c78e600318150d119f974ae41179
Michael Stiefler <michael.stiefler@gmail.com>
2021-05-31 10:53:36 +0000

    Selector Version 4.0.1 Update Fixes incompatibilities with older game versions where e.g. DT.dll doesn't exist.

--- Storebror/SAS_IL2ExecutableSelector/global/version.h
+++ Storebror/SAS_IL2ExecutableSelector/global/version.h
@@ -1,6 +1,6 @@
-#define G_FILE_VERSION "4.0.0.3 Build 21021601"
-#define G_PRODUCT_VERSION "4.0.0"
-#define G_FILEVERSION 4,0,0,3
-#define G_PRODUCTVERSION 4,0,0,3
+#define G_FILE_VERSION "4.0.1.1 Build 21053101"
+#define G_PRODUCT_VERSION "4.0.1"
+#define G_FILEVERSION 4,0,1,1
+#define G_PRODUCTVERSION 4,0,1,1
 #define G_SPECIAL_BUILD "Public Release"
-#define G_COPYRIGHT "(C) SAS February 2021"
+#define G_COPYRIGHT "(C) SAS May 2021"
