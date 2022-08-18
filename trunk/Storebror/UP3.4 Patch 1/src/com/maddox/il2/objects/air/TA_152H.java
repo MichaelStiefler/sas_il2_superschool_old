b983130e8d43af0bd485b86556f77769c6a09e56
Storebror <Storebror@cec7fac4-210f-42c9-baaf-7c315aaa1326>
2022-08-18 06:50:50 +0000

    UP 3.4 Patch 1 WIP 2022-08-18

--- trunk/Storebror/UP3.4 Patch 1/src/com/maddox/il2/objects/air/TA_152H.java
+++ trunk/Storebror/UP3.4 Patch 1/src/com/maddox/il2/objects/air/TA_152H.java
@@ -1,82 +1,13 @@
 package com.maddox.il2.objects.air;
 
-import com.maddox.il2.engine.HierMesh;
-import com.maddox.il2.objects.weapons.GunEmpty;
 import com.maddox.rts.Property;
 
 public class TA_152H extends TA_152NEW {
 
-    public TA_152H() {
-        this.kangle = 0.0F;
-    }
-
-    public static void moveGear(HierMesh hiermesh, float f) {
-        hiermesh.chunkSetAngles("GearL2_D0", 0.0F, -77F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearR2_D0", 0.0F, -77F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearL3_D0", 0.0F, -102F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearR3_D0", 0.0F, -102F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearC2_D0", 20F * f, 0.0F, 0.0F);
-        hiermesh.chunkSetAngles("GearC3_D0", 0.0F, 0.0F, 0.0F);
-        float f1 = Math.max(-f * 1500F, -94F);
-        hiermesh.chunkSetAngles("GearL5_D0", 0.0F, f1, 0.0F);
-        hiermesh.chunkSetAngles("GearR5_D0", 0.0F, f1, 0.0F);
-        Aircraft.xyz[0] = Aircraft.xyz[1] = Aircraft.xyz[2] = 0.0F;
-        Aircraft.ypr[0] = Aircraft.ypr[1] = Aircraft.ypr[2] = 0.0F;
-        Aircraft.xyz[2] = Aircraft.cvt(f, 0.0F, 1.0F, 0.0F, 0.1F);
-        hiermesh.chunkSetLocate("poleL_D0", Aircraft.xyz, Aircraft.ypr);
-        hiermesh.chunkSetLocate("poleR_D0", Aircraft.xyz, Aircraft.ypr);
-    }
-
-    protected void moveGear(float f) {
-        TA_152H.moveGear(this.hierMesh(), f);
-    }
-
-    public void moveSteering(float f) {
-        if (this.FM.CT.getGear() < 0.98F) {
-            return;
-        } else {
-            this.hierMesh().chunkSetAngles("GearC3_D0", 0.0F, -f, 0.0F);
-            return;
-        }
-    }
-
-    public void moveWheelSink() {
-        this.resetYPRmodifier();
-        Aircraft.xyz[1] = Aircraft.cvt(this.FM.Gears.gWheelSinking[0], 0.0F, 0.44F, 0.0F, 0.44F);
-        this.hierMesh().chunkSetLocate("GearL2a_D0", Aircraft.xyz, Aircraft.ypr);
-        Aircraft.xyz[1] = Aircraft.cvt(this.FM.Gears.gWheelSinking[1], 0.0F, 0.44F, 0.0F, 0.44F);
-        this.hierMesh().chunkSetLocate("GearR2a_D0", Aircraft.xyz, Aircraft.ypr);
-    }
-
     public void update(float f) {
-        for (int i = 1; i < 15; i++) {
-            this.hierMesh().chunkSetAngles("Water" + i + "_D0", 0.0F, -20F * this.kangle, 0.0F);
-        }
-
-        this.kangle = (0.95F * this.kangle) + (0.05F * this.FM.EI.engines[0].getControlRadiator());
-        if (this.FM.Loc.z > 9000D) {
-            if (!this.FM.EI.engines[0].getControlAfterburner()) {
-                this.FM.EI.engines[0].bHasAfterburnerControl = true;
-            }
-            this.FM.EI.engines[0].setAfterburnerType(2);
-        } else if (!this.FM.EI.engines[0].getControlAfterburner()) {
-            this.FM.EI.engines[0].bHasAfterburnerControl = false;
-        }
+        this.updateAfterburner();
         super.update(f);
     }
-
-    public void onAircraftLoaded() {
-        super.onAircraftLoaded();
-        this.FM.AS.wantBeaconsNet(true);
-        if (this.getGunByHookName("_CANNON03") instanceof GunEmpty) {
-            this.hierMesh().chunkVisible("20mmL1_D0", false);
-        }
-        if (this.getGunByHookName("_CANNON04") instanceof GunEmpty) {
-            this.hierMesh().chunkVisible("20mmR1_D0", false);
-        }
-    }
-
-    private float kangle;
 
     static {
         Class class1 = TA_152H.class;
--- trunk/Storebror/UP3.4 Patch 1/src/com/maddox/il2/objects/air/TA_152NEW.java
+++ trunk/Storebror/UP3.4 Patch 1/src/com/maddox/il2/objects/air/TA_152NEW.java
@@ -1,19 +1,30 @@
 package com.maddox.il2.objects.air;
 
+import java.io.IOException;
+
 import com.maddox.JGP.Point3d;
 import com.maddox.JGP.Vector3d;
 import com.maddox.il2.ai.Shot;
 import com.maddox.il2.ai.World;
+import com.maddox.il2.ai.air.Maneuver;
+import com.maddox.il2.ai.air.Pilot;
 import com.maddox.il2.engine.Actor;
 import com.maddox.il2.engine.ActorHMesh;
 import com.maddox.il2.engine.Config;
 import com.maddox.il2.engine.HierMesh;
 import com.maddox.il2.engine.Orient;
+import com.maddox.il2.fm.FlightModelMain;
+import com.maddox.il2.fm.RealFlightModel;
+import com.maddox.il2.game.AircraftHotKeys;
+import com.maddox.il2.game.HUD;
 import com.maddox.il2.game.Main3D;
 import com.maddox.il2.objects.Wreckage;
 import com.maddox.rts.CmdEnv;
 import com.maddox.rts.HotKeyCmd;
+import com.maddox.rts.NetMsgGuaranted;
+import com.maddox.rts.NetMsgInput;
 import com.maddox.rts.Property;
+import com.maddox.rts.Time;
 import com.maddox.sound.AudioStream;
 import com.maddox.sound.SoundFX;
 
@@ -34,6 +45,14 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
         if (Config.cur.ini.get("Mods", "SmokeEffect", 0) > 0) {
             this.bSmokeEffect = true;
         }
+        this.kangle = 0.0F;
+        this.k14Mode = 0;
+        this.k14WingspanType = 0;
+        this.k14Distance = 200F;
+        this.bToFire = false;
+        this.tX4Prev = 0L;
+        this.deltaAzimuth = 0.0F;
+        this.deltaTangage = 0.0F;
     }
 
     public static void prepareWeapons(Class aircraftClass, HierMesh hierMesh, String thisWeaponsName) {
@@ -109,6 +128,19 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
         } else {
             this.hierMesh().chunkVisible("HMask1_D0", this.hierMesh().isChunkVisible("Pilot1_D0"));
         }
+        if (((this.FM instanceof RealFlightModel) && ((RealFlightModel) this.FM).isRealMode()) || !flag || !(this.FM instanceof Pilot)) {
+            return;
+        }
+        Pilot pilot = (Pilot) this.FM;
+        if ((pilot.get_maneuver() == 63) && (((Maneuver) (pilot)).target != null)) {
+            Point3d point3d = new Point3d(((FlightModelMain) (((Maneuver) (pilot)).target)).Loc);
+            point3d.sub(this.FM.Loc);
+            this.FM.Or.transformInv(point3d);
+            if ((((point3d.x > 4000D) && (point3d.x < 5500D)) || ((point3d.x > 100D) && (point3d.x < 5000D) && (World.Rnd().nextFloat() < 0.33F))) && (Time.current() > (this.tX4Prev + 10000L))) {
+                this.bToFire = true;
+                this.tX4Prev = Time.current();
+            }
+        }
     }
 
     public void moveCockpitDoor(float f) {
@@ -181,6 +213,7 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
 
     public void onAircraftLoaded() {
         super.onAircraftLoaded();
+        this.FM.AS.wantBeaconsNet(true);
         if (this.FM.isPlayers()) {
             this.FM.EI.engines[0].setControlPropAuto(false);
             this.FM.CT.bHasCockpitDoorControl = true;
@@ -191,6 +224,11 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
     }
 
     public void update(float f) {
+        for (int i = 1; i < 15; i++) {
+            this.hierMesh().chunkSetAngles("Water" + i + "_D0", 0.0F, -20F * this.kangle, 0.0F);
+        }
+        this.kangle = (0.95F * this.kangle) + (0.05F * this.FM.EI.engines[0].getControlRadiator());
+        
         if ((this.FM.CT.cockpitDoorControl > 0.9F) && (this.FM.getSpeedKMH() > 240F) && !this.FM.Gears.onGround() && (this.FM == World.getPlayerFM()) && (this.hierMesh().chunkFindCheck("Blister1_D0") != -1) && (this.FM.AS.getPilotHealth(0) > 0.0F)) {
             this.arrach.setPlay(true);
             this.EjectCanopy();
@@ -309,20 +347,44 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
     }
 
     public static void moveGear(HierMesh hiermesh, float f) {
-        hiermesh.chunkSetAngles("GearL2_D0", 0.0F, 77F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearR2_D0", 0.0F, 77F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearL3_D0", 0.0F, 157F * f, 0.0F);
-        hiermesh.chunkSetAngles("GearR3_D0", 0.0F, 157F * f, 0.0F);
+        hiermesh.chunkSetAngles("GearL2_D0", 0.0F, -77F * f, 0.0F);
+        hiermesh.chunkSetAngles("GearR2_D0", 0.0F, -77F * f, 0.0F);
+        hiermesh.chunkSetAngles("GearL3_D0", 0.0F, -102F * f, 0.0F);
+        hiermesh.chunkSetAngles("GearR3_D0", 0.0F, -102F * f, 0.0F);
         hiermesh.chunkSetAngles("GearC2_D0", 20F * f, 0.0F, 0.0F);
+        hiermesh.chunkSetAngles("GearC3_D0", 0.0F, 0.0F, 0.0F);
         float f1 = Math.max(-f * 1500F, -94F);
-        hiermesh.chunkSetAngles("GearL5_D0", 0.0F, -f1, 0.0F);
-        hiermesh.chunkSetAngles("GearR5_D0", 0.0F, -f1, 0.0F);
+        hiermesh.chunkSetAngles("GearL5_D0", 0.0F, f1, 0.0F);
+        hiermesh.chunkSetAngles("GearR5_D0", 0.0F, f1, 0.0F);
+        Aircraft.xyz[0] = Aircraft.xyz[1] = Aircraft.xyz[2] = 0.0F;
+        Aircraft.ypr[0] = Aircraft.ypr[1] = Aircraft.ypr[2] = 0.0F;
+        Aircraft.xyz[2] = Aircraft.cvt(f, 0.0F, 1.0F, 0.0F, 0.1F);
+        hiermesh.chunkSetLocate("poleL_D0", Aircraft.xyz, Aircraft.ypr);
+        hiermesh.chunkSetLocate("poleR_D0", Aircraft.xyz, Aircraft.ypr);
+    }
+
+    protected void moveGear(float f) {
+        TA_152NEW.moveGear(this.hierMesh(), f);
     }
 
     public void moveSteering(float f) {
         if (this.FM.CT.getGear() >= 0.98F) {
-            this.hierMesh().chunkSetAngles("GearC2_D0", 0.0F, -f, 0.0F);
+            this.hierMesh().chunkSetAngles("GearC3_D0", 0.0F, -f, 0.0F);
         }
+    }
+
+    public void moveWheelSink() {
+        this.resetYPRmodifier();
+        Aircraft.xyz[1] = Aircraft.cvt(this.FM.Gears.gWheelSinking[0], 0.0F, 0.44F, 0.0F, 0.44F);
+        this.hierMesh().chunkSetLocate("GearL2a_D0", Aircraft.xyz, Aircraft.ypr);
+        Aircraft.xyz[1] = Aircraft.cvt(this.FM.Gears.gWheelSinking[1], 0.0F, 0.44F, 0.0F, 0.44F);
+        this.hierMesh().chunkSetLocate("GearR2a_D0", Aircraft.xyz, Aircraft.ypr);
+    }
+
+    protected void moveFlap(float f) {
+        float f1 = -50F * f;
+        this.hierMesh().chunkSetAngles("Flap01_D0", 0.0F, f1, 0.0F);
+        this.hierMesh().chunkSetAngles("Flap02_D0", 0.0F, -f1, 0.0F);
     }
 
     public void setOnGround(Point3d point3d, Orient orient, Vector3d vector3d) {
@@ -909,6 +971,104 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
 //        }
 //    }
 
+    public boolean typeFighterAceMakerToggleAutomation() {
+        this.k14Mode++;
+        if (this.k14Mode > 2) {
+            this.k14Mode = 0;
+        }
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "K14AceMakerMode" + this.k14Mode);
+        return true;
+    }
+
+    public void typeFighterAceMakerAdjDistanceReset() {
+    }
+
+    public void typeFighterAceMakerAdjDistancePlus() {
+        this.k14Distance += 10F;
+        if (this.k14Distance > 800F) {
+            this.k14Distance = 800F;
+        }
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "K14AceMakerInc");
+    }
+
+    public void typeFighterAceMakerAdjDistanceMinus() {
+        this.k14Distance -= 10F;
+        if (this.k14Distance < 200F) {
+            this.k14Distance = 200F;
+        }
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "K14AceMakerDec");
+    }
+
+    public void typeFighterAceMakerAdjSideslipReset() {
+    }
+
+    public void typeFighterAceMakerAdjSideslipPlus() {
+        this.k14WingspanType--;
+        if (this.k14WingspanType < 0) {
+            this.k14WingspanType = 0;
+        }
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "AskaniaWing" + this.k14WingspanType);
+    }
+
+    public void typeFighterAceMakerAdjSideslipMinus() {
+        this.k14WingspanType++;
+        if (this.k14WingspanType > 9) {
+            this.k14WingspanType = 9;
+        }
+        HUD.log(AircraftHotKeys.hudLogWeaponId, "AskaniaWing" + this.k14WingspanType);
+    }
+
+    public void typeFighterAceMakerReplicateToNet(NetMsgGuaranted netmsgguaranted) throws IOException {
+        netmsgguaranted.writeByte(this.k14Mode);
+        netmsgguaranted.writeByte(this.k14WingspanType);
+        netmsgguaranted.writeFloat(this.k14Distance);
+    }
+
+    public void typeFighterAceMakerReplicateFromNet(NetMsgInput netmsginput) throws IOException {
+        this.k14Mode = netmsginput.readByte();
+        this.k14WingspanType = netmsginput.readByte();
+        this.k14Distance = netmsginput.readFloat();
+    }
+    
+    public void typeX4CAdjSidePlus() {
+        this.deltaAzimuth = 1.0F;
+    }
+
+    public void typeX4CAdjSideMinus() {
+        this.deltaAzimuth = -1F;
+    }
+
+    public void typeX4CAdjAttitudePlus() {
+        this.deltaTangage = 1.0F;
+    }
+
+    public void typeX4CAdjAttitudeMinus() {
+        this.deltaTangage = -1F;
+    }
+
+    public void typeX4CResetControls() {
+        this.deltaAzimuth = this.deltaTangage = 0.0F;
+    }
+
+    public float typeX4CgetdeltaAzimuth() {
+        return this.deltaAzimuth;
+    }
+
+    public float typeX4CgetdeltaTangage() {
+        return this.deltaTangage;
+    }
+    
+    void updateAfterburner() {
+        if (this.FM.Loc.z > 9000D) {
+            if (!this.FM.EI.engines[0].getControlAfterburner()) {
+                this.FM.EI.engines[0].bHasAfterburnerControl = true;
+            }
+            this.FM.EI.engines[0].setAfterburnerType(2);
+        } else if (!this.FM.EI.engines[0].getControlAfterburner()) {
+            this.FM.EI.engines[0].bHasAfterburnerControl = false;
+        }
+    }
+
     private boolean       bSmokeEffect;
     private float         trimElevator;
     private boolean       bHasElevatorControl;
@@ -940,6 +1100,14 @@ public abstract class TA_152NEW extends Scheme1 implements TypeFighter, TypeBNZF
     protected AudioStream bouton;
     protected AudioStream arrach;
     private AudioStream   windExtFw;
+    private float kangle;
+    public boolean bToFire;
+    private long   tX4Prev;
+    public int     k14Mode;
+    public int     k14WingspanType;
+    public float   k14Distance;
+    private float  deltaAzimuth;
+    private float  deltaTangage;
 
     static {
         Class class1 = TA_152NEW.class;
